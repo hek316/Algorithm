2bed256f5637bcc45e4131cb316a40361ef15a1e
HwangEunkyung <hek3167@gmail.com>
2023-05-06 14:41:24 +0900

    BaekJoon 11656 접미사 배열 2 (Arrays 활용)

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                String word = br.readLine();
-                String[] st = new String[word.length()];
-                for(int i= 0; i < word.length(); i++) {
-                    st[i]  = word.substring(i);
-                }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        String[] st = new String[word.length()];
+        for(int i= 0; i < word.length(); i++) {
+            st[i]  = word.substring(i);
+        }
 
-                for(int i= 0; i < word.length(); i++) {
-                  for (int j= i +1; j < word.length(); j++) {
-                       int k = 0;
-                      while(k < st[i].length() && k < st[j].length()) {
-                          char a = st[i].charAt(k); char b = st[j].charAt(k);
-                          if ( a> b) {
-                              String s = st[i];
-                              st[i] = st[j];
-                              st[j] = s;
+        Arrays.sort(st);
 
-                              break;
-                          } else if (st[i].charAt(k) == st[j].charAt(k)) {
-                              k++;
-                              if (k == st[j].length()) {
-                                  String s = st[i];
-                                  st[i] = st[j];
-                                  st[j] = s;
-                              }
-                         } else {
-                              break;
-                          }
-                      }
-                  }
-                }
-                for(int i= 0; i < word.length(); i++) {
-                    System.out.println(st[i]);
-                }
+        StringBuilder sb = new StringBuilder();
+
+
+
+        for(int i= 0; i < word.length(); i++) {
+            sb.append(st[i]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
