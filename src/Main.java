92c1c56afa33beaa78d0bd354a41993026bd4b5d
hek316 <hek3167@gmail.com>
2023-05-07 13:25:24 +0900

    BaekJoon 300 - [수학1] 1934 최소공배수 (시간초과)

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    public int lcm(int num1, int num2){
+        int big ;
+        if (num1 > num2) {
+            big = num1;
+        } else {
+            big = num2;
+        }
+
+        while (true){
+            if (big % num1 == 0 && big % num2 == 0){
+                break;
+            }
+            big++;
+        }
+
+        return big;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
-        String[] s = input.split(" ");
-
-        int a = Integer.parseInt(s[0]);
-        int b = Integer.parseInt(s[1]);
-        int lcd = 0;
-        int max = 0;
-
-        if (a > b) {
-            max = a;
-        } else {
-            max = b;
-        }
-
-        for (int i=max; i > 0 ; i--) {
-            if (a%i == 0 && b%i == 0){
-                lcd = i;
-                break;
-            }
-
-        }
-
-
+        int n = Integer.parseInt(br.readLine());
+        String[] s = new String[n];
         StringBuilder sb = new StringBuilder();
-        sb.append(lcd).append("\n");
-        sb.append(a*b/lcd).append("\n");
-
+        Main main = new Main();
+        for (int i=0; i < n; i++) {
+            s[i] = br.readLine();
+            String[] num = s[i].split(" ");
+            int num1 = Integer.parseInt(num[0]);
+            int num2 =  Integer.parseInt(num[1]);
+            sb.append(main.lcm(num1, num2)).append("\n");
+        }
 
         System.out.println(sb);
+
     }
 }
