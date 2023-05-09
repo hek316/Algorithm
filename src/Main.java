77202a5a90d48c57a4aa1750d6cdcd5f9438e249
HwangEunkyung <hek3167@gmail.com>
2023-05-10 00:13:57 +0900

    BaekJoon 1929 소수 구하기 refactoring

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static boolean[] eratos(int n2){
+    public static StringBuilder eratos(int n1, int n2){
         boolean[] isCompositeNumber = new boolean[n2+1];
+        StringBuilder sb = new StringBuilder();
         for (int i = 2; i <= n2; i++) {
             if (isCompositeNumber[i]) {
                 continue;
@@ -13,8 +14,11 @@ public class Main {
             for (int j = 2; j*i <= n2; j++) {
                 isCompositeNumber[i*j] = true;
             }
+            if(i>= n1 && i <= n2){
+                sb.append(i).append("\n");
+            }
         }
-        return isCompositeNumber;
+        return sb;
 
     }
     public static void main(String[] args) throws IOException {
@@ -23,18 +27,7 @@ public class Main {
         String[] s = br.readLine().split(" ");
         int n1 = Integer.parseInt(s[0]);
         int n2 = Integer.parseInt(s[1]);
-        boolean[] isCompositeNumber = eratos(n2);
-        StringBuilder sb = new StringBuilder();
-        if (n1 == 1) {
-            n1++;
-        }
-        for (int i = n1; i <= n2; i++) {
-            if(!isCompositeNumber[i]){
-                sb.append(i).append("\n");
-            }
-
-        }
-
+        StringBuilder sb = eratos(n1, n2);
         System.out.println(sb);
     }
 }
