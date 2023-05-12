c870c25016a728fe4e9796bfc7e4667059137e44
HwangEunkyung <hek3167@gmail.com>
2023-05-12 16:14:45 +0900

    BaekJoon 6588 골드바흐의 추측 refactoring

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringBuilder sb = new StringBuilder();
-        int[] prime = new int[1000000+1];
-
         boolean[] isComposition = new boolean[1000000+1];
         for(int i=2; i<= 1000000; i++){
-            if (isComposition[i] == true) {
+            if (isComposition[i]) {
                 continue;
             }
             for(int j=2; i*j<= 1000000; j++){
                 isComposition[i*j] = true;
             }
         }
-        int j =0;
-        for(int i=2; i<= 1000000; i++){
-
-            if (isComposition[i] == false) {
-                prime[j++] = i;
-            }
-
-        }
 
         while (true){
             int n1 = Integer.parseInt(br.readLine());
             if (n1 == 0){ break;}
-
-            sb.append(n1).append(" = ");
-            for (int i = 0; i< n1 ;i++) {
-                int n2;
-                n2 = n1 - prime[i];
-                if(isComposition[n2] == false){
-                    sb.append(prime[i]).append(" + ").append(n2).append("\n");
+            for (int i = 2; i< n1 ;i++) {
+                if(!isComposition[i] &&  !isComposition[n1 - i]){
+                    sb.append(n1).append(" = ").append(i).append(" + ").append(n1 - i).append("\n");
                     break;
                 }
 
