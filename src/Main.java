13b8594bf53d6ef816d53b242252b143caee36de
HwangEunkyung <hek3167@gmail.com>
2023-05-17 22:48:52 +0900

    BaekJoon 2004 조합 0의 개수

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    long countMultiple(int n1, int divNum){
+        long cnt = 0;
+        /*
+         while ( m != 0) {
+            cnt = cnt + m ;
+            i*=mutiple;
+            m= n1/i;
+        }
+        초기 풀이시 나누는 값을 곱하는 형식으로 풀었는데 해당 식은 overFlow 가 발생한다
+        int 의 범위 :
+        –2,147,483,648 ~ 2,147,483,647
+         */
+        while ( n1/divNum != 0) {
+            cnt += n1/divNum;
+            n1/=divNum;
+        }
+        return cnt;
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        String st = br.readLine();
+        String[] a = st.split(" ");
+        int n1 = Integer.parseInt(a[0]);
+        int n2 = Integer.parseInt(a[1]);
+        Main main = new Main();
 
-        int cnt = 0;
-        for (int i = n; i > 0; i--) {
-            int k = i;
-            while (k%5==0) {
-                cnt++;
-                k/=5;
-            }
+        final int DIV2 = 2;
+        final int DIV5 = 5;
+        long cnt2 = main.countMultiple(n1,DIV2) - main.countMultiple(n2, DIV2)  - main.countMultiple((n1 -n2), DIV2);
+        long cnt5 = main.countMultiple(n1,DIV5) - main.countMultiple(n2, DIV5)  - main.countMultiple((n1 -n2), DIV5);
+        if (cnt2 < cnt5) {
+            System.out.println(cnt2);
+        } else {
+            System.out.println(cnt5);
         }
-        System.out.println(cnt);
-
     }
 }
