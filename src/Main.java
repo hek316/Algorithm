757c953061a57bfc89291829d39d50f53504280a
HwangEunkyung <hek3167@gmail.com>
2023-05-20 21:09:40 +0900

    BaekJoon 17087 숨바꼭질6 (push 오류로 인한 재push)

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    long countMultiple(int n1, int divNum){
-        long cnt = 0;
-        /*
-         while ( m != 0) {
-            cnt = cnt + m ;
-            i*=mutiple;
-            m= n1/i;
-        }
-        초기 풀이시 나누는 값을 곱하는 형식으로 풀었는데 해당 식은 overFlow 가 발생한다
-        int 의 범위 :
-        –2,147,483,648 ~ 2,147,483,647
-         */
-        while ( n1/divNum != 0) {
-            cnt += n1/divNum;
-            n1/=divNum;
-        }
-        return cnt;
-    }
+import java.util.StringTokenizer;
 
+public class Main {
+
+    public static int getGCD(int n1 , int n2) {
+        if (n2 == 0){
+            return n1;
+        }
+        if (n1 > n2) {
+            return getGCD(n2 ,n1%n2);
+        }else {
+            return getGCD(n1 ,n2%n1);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String st = br.readLine();
-        String[] a = st.split(" ");
-        int n1 = Integer.parseInt(a[0]);
-        int n2 = Integer.parseInt(a[1]);
-        Main main = new Main();
+        String s1 = br.readLine();
+        StringTokenizer st = new StringTokenizer(s1);
+        int n = Integer.parseInt(st.nextToken());
+        int mark = Integer.parseInt(st.nextToken());
 
-        final int DIV2 = 2;
-        final int DIV5 = 5;
-        long cnt2 = main.countMultiple(n1,DIV2) - main.countMultiple(n2, DIV2)  - main.countMultiple((n1 -n2), DIV2);
-        long cnt5 = main.countMultiple(n1,DIV5) - main.countMultiple(n2, DIV5)  - main.countMultiple((n1 -n2), DIV5);
-        if (cnt2 < cnt5) {
-            System.out.println(cnt2);
-        } else {
-            System.out.println(cnt5);
+        String s2 = br.readLine();
+        StringTokenizer st2 = new StringTokenizer(s2);
+
+        int gcd =Math.abs(Integer.parseInt(st2.nextToken()) - mark);
+        for (int i=1; i < n; i++) {
+            gcd = getGCD(Math.abs(Integer.parseInt(st2.nextToken()) - mark), gcd);
         }
+
+        System.out.println(gcd);
+
     }
 }
