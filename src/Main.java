2f7c31d97065c6dbdcff0b2f1da391f3c985d933
HwangEunkyung <hek3167@gmail.com>
2023-05-22 19:07:24 +0900

    BaekJoon 1463 1로 만들기 Bottom-up

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class Main {
 
-    public static int makeOne(int n, int[] d){
+    static int  makeOne(int n1, int[] d){
 
-        if (n==1){
-            return 0;
+        for(int i=2; i<= n1; i++){
+            d[i] = 1 + d[i-1];
+            if(i%2 == 0){
+                int temp = 1 + d[i/2];
+                if (temp < d[i]) {
+                    d[i]  = temp;
+                }
+            }
+
+            if(i%3 == 0){
+                int temp = 1 + d[i/3];
+                if (temp < d[i]) {
+                    d[i]  = temp;
+                }
+            }
         }
-        if (d[n] > 0){
-            return d[n];
-        }
-
-        d[n] =  makeOne(n-1, d) +1;
-
-        if (n%2 ==0) {
-            int tmp = makeOne(n/2, d)+1;
-            d[n]  = d[n] < tmp ? d[n]: tmp;
-        }
-
-
-        if (n%3 ==0) {
-            int tmp = makeOne(n/3, d)+1;
-            d[n]  = d[n] < tmp ? d[n]: tmp;
-        }
-
-        return d[n];
+        return d[n1];
     }
-
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] d = new int[n+1];
-        System.out.println(makeOne(n, d));
+        int n1 = Integer.parseInt(br.readLine());
+        int[] d = new int[n1+1];
+        d[1] = 0;
+        System.out.println(makeOne(n1, d));
+
     }
 }
