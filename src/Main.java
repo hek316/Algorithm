eaa906607c4548f091a0bb5fd51c54e49a0f0a10
hek316 <hek3167@gmail.com>
2023-05-23 15:19:40 +0900

    BaekJoon 1463 1로 만들기 재귀호출

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+    /*
+        ex 11 일 경우 11 -> 10 -> 9   두번 빼고 3으로 나누므로 나머지 2더함
+         10 일 경우 -> 9 한번 빼고 3으로 나누므로 나머지 1 더함
+     */
 
-    static long  GCD(int n1, int n2){
-        int min = n1;
-        if (n2 < n1) {
-            min = n2;
-        }
-        long gcd = 1;
-        for (int i = min; i > 0; i--) {
-            if (n1%i ==0 && n2%i ==0) {
-                gcd = i;
-                break;
-            }
+    static int  makeOne(int n1, int cnt){
 
+        if (n1 < 2){
+            return cnt;
         }
-        return gcd;
+        return  Math.min(makeOne(n1/2, cnt +1 + n1%2),makeOne(n1/3, cnt +1 + n1%3) );
+
     }
     public static void main(String[] args) throws IOException {
-         /*
-          합이 int 의 값의 범위를 초과할 수 있으므로 long  형으로 선언
-          */
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n1 = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while (n1--> 0){
-            String st = br.readLine();
-            String[] s = st.split(" ");
-            int cnt =Integer.parseInt(s[0]);
-            int[] num = new int[cnt];
-            for (int i=0; i < cnt; i++) {
-                num[i] = Integer.parseInt(s[i+1]);
-            }
-
-            long sum = 0;
-            for (int i=0; i < num.length; i++) {
-                for (int j=i +1; j < num.length; j++) {
-                    sum +=GCD(num[i], num[j]);
-                }
-            }
-            sb.append(sum).append("\n");
-        }
-        System.out.println(sb);
+        int cnt =0;
+        System.out.println(makeOne(n1, cnt));
 
     }
 }
