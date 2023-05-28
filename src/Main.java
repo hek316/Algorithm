8c850bdbe883610c0ce0323a217d9ea80b9deeb4
hek316 <hek3167@gmail.com>
2023-05-28 11:58:39 +0900

    BaekJoon 400 - [다이나믹 프로그래밍 1] 11726 2×n 타일링 (Top-down)

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class Main {
-
-    static long  GCD(int n1, int n2){
-        int min = n1;
-        if (n2 < n1) {
-            min = n2;
-        }
-        long gcd = 1;
-        for (int i = min; i > 0; i--) {
-            if (n1%i ==0 && n2%i ==0) {
-                gcd = i;
-                break;
-            }
-
-        }
-        return gcd;
-    }
-    public static void main(String[] args) throws IOException {
-         /*
-          합이 int 의 값의 범위를 초과할 수 있으므로 long  형으로 선언
-          */
+/*
+        초기에 조합의 성질을 이용하여 풀려고 했으나 곱하는 수가 점점 커져서 범위를 초과하였음
+        그래서 피보나치 수열 성질을 이용하여 풀이 하였음
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n1 = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while (n1--> 0){
-            String st = br.readLine();
-            String[] s = st.split(" ");
-            int cnt =Integer.parseInt(s[0]);
-            int[] num = new int[cnt];
-            for (int i=0; i < cnt; i++) {
-                num[i] = Integer.parseInt(s[i+1]);
-            }
+        int n = Integer.parseInt(br.readLine());
+        int m = n/2;
 
-            long sum = 0;
-            for (int i=0; i < num.length; i++) {
-                for (int j=i +1; j < num.length; j++) {
-                    sum +=GCD(num[i], num[j]);
-                }
+        long cnt = 0;
+        for (int i= 0; i <= m; i++) {
+            int r  = n - 2*i;;
+            long tmp = 1;
+
+            int k = r+i;
+            for(int j = r+1; j <= k; j++){
+                tmp *= j;
             }
-            sb.append(sum).append("\n");
+            int ir = k - r;
+            for(int j = 1; j <= ir; j++){
+                tmp/= j;
+            }
+            cnt += tmp;
+            cnt%=10007;
         }
-        System.out.println(sb);
+        System.out.println(cnt);
+
+ */
+public class Main {
+
+
+    public int f(int[] d, int n){
+        if (d[n] > 0) {
+            return d[n];
+        }
+        if(n ==1) {
+            d[1] = 1;
+            return 1;
+        }else if(n ==2) {
+            d[2] = 2;
+            return 2;
+        }
+        d[n] =  f(d, n-2) + f(d, n-1);
+        return d[n]%10007;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] d = new int[n+1];
+        Main main = new Main();
+        int f = main.f(d, n);
+        System.out.println(f);
 
     }
 }
