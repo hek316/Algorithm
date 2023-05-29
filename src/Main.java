ec9fed5504984c1ba296e832977c9f581f200f0e
HwangEunkyung <hek3167@gmail.com>
2023-05-29 19:23:35 +0900

    BaekJoon 11727 2×n 타일링 2 top-down

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] d = new int[n+2];
-        d[1] = 1;
-        d[2] = 2;
-        for (int i=3; i <= n; i++) {
-            d[i] = (d[i-2] + d[i-1])%10007;
+    public int solve2Ntile(int[] d,int n){
+        if (d[n] > 0) {
+            return d[n];
         }
-        System.out.println(d[n]);
+
+        if (n == 1) {
+            d[n] = 1;
+            return d[n];
+        } else if (n == 2) {
+            d[n] = 3;
+            return d[n];
+        }
+        d[n] = (solve2Ntile(d,n-1) + solve2Ntile(d,n-2)*2)%10007;
+        return d[n];
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Main main = new Main();
+        int[] d = new int[n+2];
+        int i = main.solve2Ntile(d, n);
+        System.out.println(i);
+
     }
 }
