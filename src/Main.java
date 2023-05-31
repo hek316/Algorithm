489a559a49f1ceed582746a51dd1a944bcabcb49
hek316 <hek3167@gmail.com>
2023-05-31 12:54:32 +0900

    BaekJoon 11052 카드 구매하기 bottom-up

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,53 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public int plus123(int[] d,int n){
-        if(d[n] > 0){
-            return d[n];
+    public int f(int[] arr, int n1, int[] ans){
+
+        ans[1] =arr[1];
+        ans[2] =  arr[1]*2 > arr[2] ? arr[1]*2 : arr[2];
+
+
+        for(int i=3; i<=n1; i++){
+            int tmp = i/2;
+            if(i%2!=0){
+                tmp++;
+            }
+            int max = arr[i];
+            for (int j = i-1; j >= tmp; j--) {
+                int tmp2 = ans[j] + ans[i-j];
+                if (max < tmp2) {
+                    max = tmp2;
+                }
+            }
+            ans[i] = max;
+
+
         }
-        d[1] = 1;
-        d[2] = 2;
-        d[3] = 4;
-        for(int i=4; i<=n; i++){
-            d[i] = d[i-1] + d[i-2] + d[i-3];
-        }
-        return d[n];
+        return ans[n1];
     }
-    public static void main(String[] args) throws Exception {
+
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n1 = Integer.parseInt(br.readLine());
+        int[] arr = new int[n1+2];
+        int[] ans = new int[n1+2];
+
+        String s = br.readLine();
         Main main = new Main();
-        int[] d = new int[12];
-        StringBuilder sb = new StringBuilder();
-        for (int i=0; i < n; i++) {
-            int n1 = Integer.parseInt(br.readLine());
-            sb.append(main.plus123(d, n1)).append("\n");
+
+        StringTokenizer st = new StringTokenizer(s);
+
+        for(int i=1; i<=n1; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(sb);
-
+        System.out.println(main.f(arr,n1, ans));
     }
 }
