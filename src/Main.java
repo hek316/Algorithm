146e4a9992cbf87a4780ed3b99b553af502fb1f4
HwangEunkyung <hek3167@gmail.com>
2023-05-31 21:59:22 +0900

    BaekJoon 9095 1, 2, 3 더하기  top-down

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public int f(int[] arr, int n1, int[] ans){
 
-        ans[1] =arr[1];
-        ans[2] =  arr[1]*2 > arr[2] ? arr[1]*2 : arr[2];
+        if (n1 == 1) {
+            return arr[1];
+        }else if (n1 == 2){
+            if(ans[2] > 0){
+                return ans[2];
+            }else {
+                ans[2] =  arr[1]*2 > arr[2] ? arr[1]*2 : arr[2];
+                return ans[2];
+            }
+        }
+        if(ans[n1] >0){
+            return ans[n1];
+        }
 
-
-        for(int i=3; i<=n1; i++){
+        for(int i=n1; i>1; i--){
             int tmp = i/2;
             if(i%2!=0){
                 tmp++;
             }
             int max = arr[i];
+
             for (int j = i-1; j >= tmp; j--) {
-                int tmp2 = ans[j] + ans[i-j];
+                int tmp2 = f(arr, j,  ans) + f(arr, i-j,  ans);
                 if (max < tmp2) {
                     max = tmp2;
                 }
             }
             ans[i] = max;
-
-
         }
         return ans[n1];
     }
