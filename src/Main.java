58f5e9139accf046d9a37e22a9aff7fc7a1c9850
hek316 <yhr05008@naver.com>
2023-07-03 23:04:09 +0900

    BaekJoon 2193 이친수

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,23 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
-/*
-  int[] arr = new int[ n + 1];
-        int[] tmp = new int[n*1000];
-        int k =0;
-        for(int i=1; i<=9;i++){
-            arr[1] = i;
-            tmp[k++] = i;
-            for(int j=2; j<=n;j++){
-                if(arr[j-1] != 0){
-                    arr[j] = arr[j-1]-1;
-                    if(j==n){
-                        tmp[k++] = arr[j];
-
-                        System.out.println( "arr[0] :" + Arrays.toString(arr));
-                    }
-
-                }
-
-                if(arr[j-1] != 9){
-                    arr[j] = arr[j-1]+1;
-                    if(j==n){
-                        tmp[k++] = arr[j];
-                        System.out.println("arr[0] :" + Arrays.toString(arr));
-                    }
-                }
-
-            }
-
-        }
-        System.out.println("tmp:" + Arrays.toString(tmp));
-        for(int s= tmp.length-1; s>=0; s--){
-            if(tmp[s]!= 0){
-                System.out.println(tmp[s]);
-                break;
-            }
-        }
-처음에는 마지막 수 배열을 저장하는 방법을 생각했으나 메모리 사이즈가 초과하여
-연속이라는 속성에 맞춰 점화식을 생성하여 품
- */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int mod = 1000000000;
+        long[] arr = new long[n+2];
 
-        long[][] arr = new long[101][10];
-        for(int i=1; i< 10; i++){
-            arr[1][i] = 1;
-        }
-        for(int j=2; j<=n; j++){
-            for(int i=0; i<=9; i++){
-                if(i== 0){
-                    arr[j][i] = arr[j-1][i+1]%mod;
-                } else if (i==9) {
-                    arr[j][i] = arr[j-1][i-1]%mod;
-                } else {
-                    arr[j][i] = arr[j-1][i+1]%mod+ arr[j-1][i-1]%mod;
-                }
-            }
-        }
-        long ans = 0;
-        for(int k =0; k<=9; k++){
-            ans += arr[n][k];
-            ans%=mod;
-        }
-        System.out.println(ans);
+        arr[1] = 1;
+        arr[2] = 1;
 
+        for (int i=3; i<=n; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+
+        System.out.println(arr[n]);
 
     }
 }
