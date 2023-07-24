af127dec79d2f35e3c2a587f55d73618df5bfa20
hek316 <yhr05008@naver.com>
2023-07-24 23:09:39 +0900

    BaekJoon 14002 가장 긴 증가하는 부분 수열 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
@@ -13,6 +14,9 @@ public class Main {
         int[] ar = new int[n];
         // 자신보다 왼쪽에 있는 작은수 담기
         int[] cntAr = new int[n];
+        // 영향받은 그 이전 인덱스 담기
+        int[] idx = new int[n];
+
 
         for(int i=0; i < n; i++){
             ar[i] =Integer.parseInt(s[i]);
@@ -24,6 +28,7 @@ public class Main {
             for (int j=i-1; j>=0; j--) {
                 if(ar[j] < ar[i] && cntAr[i] <= cntAr[j] +1){
                     cntAr[i] = cntAr[j] +1;
+                    idx[i] = j;
                 }
             }
             if(cntAr[i] == 0){
@@ -31,12 +36,31 @@ public class Main {
             }
         }
         int max = 0;
+        int maxIdx = 0;
         for(int i=0; i < n; i++){
             if(max < cntAr[i]){
                 max = cntAr[i];
+                maxIdx = i;
             }
         }
-        System.out.println(max);
+
+
+        StringBuilder sb = new StringBuilder();
+
+        int[] val = new int[cntAr[maxIdx]];
+        int tmp = maxIdx;
+        for(int i =(cntAr[maxIdx]-1); i >=0; i-- ){
+            val[i] = ar[tmp];
+            tmp = idx[tmp];
+
+        }
+
+        sb.append(max).append("\n");
+        for(int i= 0; i < val.length; i++){
+            sb.append(val[i]).append(" ");
+        }
+
+        System.out.println(sb);
 
     }
 }
