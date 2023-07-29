4f4cbf6ac913692fd54650772ac55bac03ed8ba9
hek316 <hek3167@gmail.com>
2023-07-30 02:12:40 +0900

    BaekJoon 1912 - [다이나믹 프로그래밍 1] 연속합

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.StringTokenizer;
 
-public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+public class Main{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        String[] s = br.readLine().split(" ");
-        int[] ar = new int[n];
-        // 자신보다 왼쪽에 있는 작은수 담기
-        int[] cntAr = new int[n];
-        // 영향받은 그 이전 인덱스 담기
-        int[] idx = new int[n];
-
-
-        for(int i=0; i < n; i++){
-            ar[i] =Integer.parseInt(s[i]);
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        int[] arr = new int[n];
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        cntAr[0] =1;
+        ans[0] = arr[0];
+        int max = ans[0];;
 
-        for (int i=1; i<n; i++) {
-            for (int j=i-1; j>=0; j--) {
-                if(ar[j] < ar[i] && cntAr[i] <= cntAr[j] +1){
-                    cntAr[i] = cntAr[j] +1;
-                    idx[i] = j;
-                }
+        for(int i=1; i< n; i++){
+            if(ans[i-1] < 0){
+                ans[i] = arr[i];
+            }else{
+                ans[i] = ans[i-1] + arr[i];
             }
-            if(cntAr[i] == 0){
-                cntAr[i] = 1;
+            if(max < ans[i]){
+                max = ans[i];
             }
         }
-        int max = 0;
-        int maxIdx = 0;
-        for(int i=0; i < n; i++){
-            if(max < cntAr[i]){
-                max = cntAr[i];
-                maxIdx = i;
-            }
-        }
-
-
-        StringBuilder sb = new StringBuilder();
-
-        int[] val = new int[cntAr[maxIdx]];
-        int tmp = maxIdx;
-        for(int i =(cntAr[maxIdx]-1); i >=0; i-- ){
-            val[i] = ar[tmp];
-            tmp = idx[tmp];
-
-        }
-
-        sb.append(max).append("\n");
-        for(int i= 0; i < val.length; i++){
-            sb.append(val[i]).append(" ");
-        }
-
-        System.out.println(sb);
+        System.out.println(max);
 
     }
 }
