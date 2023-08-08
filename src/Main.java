e554b20633a7063b950fa904c7fdc6740417ff24
hek316 <hek3167@gmail.com>
2023-08-08 09:04:58 +0900

    BaekJoon 2225-[다이나믹 프로그래밍 1] 합분해 2. 직관적으로 보이는 규칙을 적용하여 식을 유도한 풀이

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,17 @@ public class Main{
         int k = Integer.parseInt(st.nextToken());
         int mod = 1000000000;
 
-        long[][] arr = new long[n+1][k+1];
-        for(int i = 0; i <=n; i++){
-            arr[i][1] = 1;
+        long[][] arr = new long[k+1][n+1];
+        for(int i = 1; i <=k; i++){
+            arr[i][0] = 1;
         }
-        for(int i = 2; i <= k; i++){
-            for(int j = 0;  j <= n; j++) {
-                for(int l = 0;  l <= j; l++) {
-                    arr[j][i] += arr[l][i - 1]%mod;
-                }
+        for(int i = 1; i <=n; i++){
+            arr[1][i] = 1;
+            for(int j = 2;  j <= k; j++) {
+                arr[j][i] = (arr[j-1][i] +  arr[j][i - 1])%mod;
             }
         }
 
-        System.out.println(arr[n][k]%mod);
+        System.out.println(arr[k][n]%mod);
     }
 }
