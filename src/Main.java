9055347d422432b8dee42f4c42fa0f6487593ef6
hek316 <yhr05008@naver.com>
2023-08-16 23:54:29 +0900

    BaekJoon 1149 - [다이나믹 프로그래밍 1(연습)] RGB거리 _top-down

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    private static int read() throws Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int c, N = System.in.read() - 48;
-        while ((c = System.in.read()) > 32) N = 10 * N + c - 48;
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n+1][3];
+        int[][] sum = new int[n+1][3];
 
-        return N;
-
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        int  n = read();
-        int[][] arr = new int[n + 1][3];
-        int[][] sum = new int[n + 1][3];
-
-        for (int i = 1; i <= n; i++) {
-            int j = 0;
-            while (j<3) {
-                arr[i][j++] = read();
+        for (int i=1; i<=n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int j =0;
+            while(st.hasMoreTokens()){
+                arr[i][j++] = Integer.parseInt(st.nextToken());
             }
         }
-
-        sum[2][0] = Math.min(arr[1][1], arr[1][2] ) + arr[2][0];
-        sum[2][1] = Math.min(arr[1][0], arr[1][2] ) + arr[2][1];
-        sum[2][2] = Math.min(arr[1][0], arr[1][1] ) + arr[2][2];
-        for (int a = 3; a <= n; a++) {
-            sum[a][0] = Math.min(sum[a - 1][1], sum[a - 1][2]) + arr[a][0];
-            sum[a][1] = Math.min(sum[a - 1][0], sum[a - 1][2]) + arr[a][1];
-            sum[a][2] = Math.min(sum[a - 1][0], sum[a - 1][1]) +arr[a][2];
-        }
-
-        int min = Math.min(sum[n][0], Math.min(sum[n][1], sum[n][2]));
+        int n1= findMinimumRGB(sum,arr,n,0);
+        int n2= findMinimumRGB(sum,arr,n,1);
+        int n3= findMinimumRGB(sum,arr,n,2);
+        int min = Math.min(n1,n2);
+        min = Math.min(min,n3);
         System.out.println(min);
     }
 
+    public static int findMinimumRGB(int[][] sum,int[][] arr, int a, int b){
+        if(b==0){
+            if (a== 2){
+                return sum[a][b]=  (arr[a-1][1] < arr[a-1][2] ? arr[a-1][1] : arr[a-1][2]) + arr[a][b];
+            }
+            if(sum[a-1][1] > 0 && sum[a-1][2] >0 ){
+                return sum[a][b]= (sum[a-1][1] < sum[a-1][2] ? sum[a-1][1] : sum[a-1][2] )+ arr[a][b];
+            } else {
+                int sum1 = findMinimumRGB(sum, arr,a-1, 1);
+                int sum2 = findMinimumRGB(sum, arr,a-1, 2);
+                return  sum[a][b]= (sum1 < sum2? sum1 : sum2)+arr[a][b];
+            }
+        } else if (b==1) {
+            if (a== 2){
+                return sum[a][b]=  (arr[a-1][0]  < arr[a-1][2] ? arr[a-1][0] : arr[a-1][2]) + arr[a][b];
+            }
+            if(sum[a-1][0] > 0 && sum[a-1][2] >0 ){
+                return sum[a][b]= (sum[a-1][0] < sum[a-1][2] ? sum[a-1][0] : sum[a-1][2]) + arr[a][b];
+            } else {
+                int sum1 = findMinimumRGB(sum, arr,a-1, 0);
+                int sum2 = findMinimumRGB(sum, arr,a-1, 2);
+                return sum[a][b]=  (sum1 < sum2? sum1 : sum2)+arr[a][b];
+            }
+        }else {
+            if (a== 2){
+                return sum[a][b]=  (arr[a-1][0] <  arr[a-1][1] ? arr[a-1][0] : arr[a-1][1]) + arr[a][b];
+            }
+            if(sum[a-1][0] > 0 && sum[a-1][1] >0 ){
+                return sum[a][b]=(sum[a-1][0] < sum[a-1][1] ? sum[a-1][0] : sum[a-1][1]) + +arr[a][b];
+            } else {
+                int sum1 = findMinimumRGB(sum, arr,a-1, 0);
+                int sum2 = findMinimumRGB(sum, arr,a-1, 1);
+                return sum[a][b]= (sum1 < sum2? sum1 : sum2)+arr[a][b];
+            }
+        }
+    }
 }
