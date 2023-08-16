9ba1bca3593ef1c539ae4f0e77a2e66336cfb00f
hek316 <yhr05008@naver.com>
2023-08-16 23:53:30 +0900

    BaekJoon 1149 - [다이나믹 프로그래밍 1(연습)] RGB거리 _bottom-up_풀이2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
         int c, N = System.in.read() - 48;
         while ((c = System.in.read()) > 32) N = 10 * N + c - 48;
+
         return N;
 
     }
@@ -11,24 +12,26 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         int  n = read();
+        int[][] arr = new int[n + 1][3];
+        int[][] sum = new int[n + 1][3];
 
-        int r = read();
-        int g = read();
-        int b = read();
-
-
-        for (int i = 2; i <= n; i++) {
-            int r1 = Math.min(g, b);
-            int g1 = Math.min(r, b);
-            int b1 =  Math.min(r, g);
-
-            r =  r1 + read();
-            g =  g1 + read();
-            b =  b1 + read();
-
+        for (int i = 1; i <= n; i++) {
+            int j = 0;
+            while (j<3) {
+                arr[i][j++] = read();
+            }
         }
 
-        int min = Math.min(r, Math.min(g, b));
+        sum[2][0] = Math.min(arr[1][1], arr[1][2] ) + arr[2][0];
+        sum[2][1] = Math.min(arr[1][0], arr[1][2] ) + arr[2][1];
+        sum[2][2] = Math.min(arr[1][0], arr[1][1] ) + arr[2][2];
+        for (int a = 3; a <= n; a++) {
+            sum[a][0] = Math.min(sum[a - 1][1], sum[a - 1][2]) + arr[a][0];
+            sum[a][1] = Math.min(sum[a - 1][0], sum[a - 1][2]) + arr[a][1];
+            sum[a][2] = Math.min(sum[a - 1][0], sum[a - 1][1]) +arr[a][2];
+        }
+
+        int min = Math.min(sum[n][0], Math.min(sum[n][1], sum[n][2]));
         System.out.println(min);
     }
 
