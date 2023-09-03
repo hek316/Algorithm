d7a3f2eaa826054794ebd4ef52e160f24aa05359
hek316 <yhr05008@naver.com>
2023-09-03 23:26:06 +0900

    BaekJoon 2156 - [다이나믹 프로그래밍 1(연습)] 포도주 시식

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,40 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        int n = read();
-
-        StringBuilder sb = new StringBuilder();
-        while (n-- > 0) {
-            int k = read();
-            int[][] dp = new int[2][100000];
-
-            for(int i=0; i<k; i++)  dp[0][i] = read();
-            for(int i=0; i<k; i++)  dp[1][i] = read();
-
-            dp[0][1] += dp[1][0];
-            dp[1][1] += dp[0][0];
-
-            for (int j=2; j< k; j++){
-                int max = Math.max(dp[0][j-2],dp[1][j-2]);
-                dp[0][j] += Math.max(dp[1][j-1], max) ;
-                dp[1][j] += Math.max(dp[0][j-1], max);
-            }
-            sb.append(Math.max(dp[0][k-1],dp[1][k-1])).append("\n");
+        int n =readInt();
+        int[][] cnt = new int[3][n];
+        for(int i=0; i<n; i++){
+            cnt[1][i] = readInt();
         }
-        System.out.println(sb);
 
+
+        int max = 0;
+
+        for(int i=1; i<n; i++){
+            max = Math.max(Math.max(cnt[0][i-1],cnt[1][i-1]),cnt[2][i-1]);
+            cnt[0][i] = max ;
+            int tmp = cnt[1][i];
+            cnt[1][i] = cnt[0][i-1] + tmp ;
+            cnt[2][i] = cnt[1][i-1] + tmp ;
+        }
+
+        max = Math.max(Math.max(cnt[0][n-1],cnt[1][n-1]),cnt[2][n-1]);
+        System.out.println(max);
     }
 
-    private static int read() throws Exception {
-
-        int c, n = System.in.read() & 15;
-
-        while ((c = System.in.read()) > 32)
-            n = (n << 3) + (n << 1) + (c & 15);
-
-        return n;
-
+    static int readInt() throws IOException {
+        int sum = 0;
+        boolean isNegative = false;
+        while(true){
+            int input = System.in.read();
+            if(input=='\n' || input==' ')
+                return isNegative ? sum*-1 : sum;
+            else if(input=='-')
+                isNegative = true;
+            else
+                sum = (sum*10)+input-'0';
+        }
     }
-
 }
