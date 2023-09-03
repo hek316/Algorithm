2a5dbf1ff7ae57fc17eaf68b130b810846606bb8
hek316 <yhr05008@naver.com>
2023-09-03 16:31:36 +0900

    BaekJoon 11057 - [다이나믹 프로그래밍 1(연습)] 오르막 수

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n+1][3];
-        int[][] sum = new int[n+1][3];
+        int mod = 10007;
 
-        for (int i=1; i<=n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int j =0;
-            while(st.hasMoreTokens()){
-                arr[i][j++] = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[10][n+1];
+        for(int i=0; i<=9; i++){
+            dp[i][1] = 1;
+        }
+
+        for (int i=2; i<=n; i++) {
+            for (int k=0; k<=9; k++) {
+                for(int j=0; j<=k; j++){
+                    dp[k][i] += dp[j][i-1];
+                    dp[k][i] %= mod;
+                }
             }
         }
-        int n1= findMinimumRGB(sum,arr,n,0);
-        int n2= findMinimumRGB(sum,arr,n,1);
-        int n3= findMinimumRGB(sum,arr,n,2);
-        int min = Math.min(n1,n2);
-        min = Math.min(min,n3);
-        System.out.println(min);
-    }
 
-    public static int findMinimumRGB(int[][] sum,int[][] arr, int a, int b){
-        if(b==0){
-            if (a== 2){
-                return sum[a][b]=  (arr[a-1][1] < arr[a-1][2] ? arr[a-1][1] : arr[a-1][2]) + arr[a][b];
-            }
-            if(sum[a-1][1] > 0 && sum[a-1][2] >0 ){
-                return sum[a][b]= (sum[a-1][1] < sum[a-1][2] ? sum[a-1][1] : sum[a-1][2] )+ arr[a][b];
-            } else {
-                int sum1 = findMinimumRGB(sum, arr,a-1, 1);
-                int sum2 = findMinimumRGB(sum, arr,a-1, 2);
-                return  sum[a][b]= (sum1 < sum2? sum1 : sum2)+arr[a][b];
-            }
-        } else if (b==1) {
-            if (a== 2){
-                return sum[a][b]=  (arr[a-1][0]  < arr[a-1][2] ? arr[a-1][0] : arr[a-1][2]) + arr[a][b];
-            }
-            if(sum[a-1][0] > 0 && sum[a-1][2] >0 ){
-                return sum[a][b]= (sum[a-1][0] < sum[a-1][2] ? sum[a-1][0] : sum[a-1][2]) + arr[a][b];
-            } else {
-                int sum1 = findMinimumRGB(sum, arr,a-1, 0);
-                int sum2 = findMinimumRGB(sum, arr,a-1, 2);
-                return sum[a][b]=  (sum1 < sum2? sum1 : sum2)+arr[a][b];
-            }
-        }else {
-            if (a== 2){
-                return sum[a][b]=  (arr[a-1][0] <  arr[a-1][1] ? arr[a-1][0] : arr[a-1][1]) + arr[a][b];
-            }
-            if(sum[a-1][0] > 0 && sum[a-1][1] >0 ){
-                return sum[a][b]=(sum[a-1][0] < sum[a-1][1] ? sum[a-1][0] : sum[a-1][1]) + +arr[a][b];
-            } else {
-                int sum1 = findMinimumRGB(sum, arr,a-1, 0);
-                int sum2 = findMinimumRGB(sum, arr,a-1, 1);
-                return sum[a][b]= (sum1 < sum2? sum1 : sum2)+arr[a][b];
-            }
+        int result = 0;
+        for(int j=0; j<=9; j++){
+            result += dp[j][n];
+            result %= mod;
         }
+        System.out.println(result);
+
     }
 }
