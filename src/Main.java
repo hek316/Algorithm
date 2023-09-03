1bc0c7d9d5bfe3f72b15b3fbae4d104ffcc86e4e
hek316 <yhr05008@naver.com>
2023-09-03 20:55:10 +0900

    BaekJoon 9465 - [다이나믹 프로그래밍 1(연습)] 스티커

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int mod = 10007;
+        int n = read();
 
-        int[][] dp = new int[10][n+1];
-        for(int i=0; i<=9; i++){
-            dp[i][1] = 1;
-        }
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            int k = read();
+            int[][] dp = new int[2][100000];
 
-        for (int i=2; i<=n; i++) {
-            for (int k=0; k<=9; k++) {
-                for(int j=0; j<=k; j++){
-                    dp[k][i] += dp[j][i-1];
-                    dp[k][i] %= mod;
-                }
+            for(int i=0; i<k; i++)  dp[0][i] = read();
+            for(int i=0; i<k; i++)  dp[1][i] = read();
+
+            dp[0][1] += dp[1][0];
+            dp[1][1] += dp[0][0];
+
+            for (int j=2; j< k; j++){
+                int max = Math.max(dp[0][j-2],dp[1][j-2]);
+                dp[0][j] += Math.max(dp[1][j-1], max) ;
+                dp[1][j] += Math.max(dp[0][j-1], max);
             }
+            sb.append(Math.max(dp[0][k-1],dp[1][k-1])).append("\n");
         }
-
-        int result = 0;
-        for(int j=0; j<=9; j++){
-            result += dp[j][n];
-            result %= mod;
-        }
-        System.out.println(result);
+        System.out.println(sb);
 
     }
+
+    private static int read() throws Exception {
+
+        int c, n = System.in.read() & 15;
+
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+
+    }
+
 }
