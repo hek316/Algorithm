729fc6e2c259a66a5ac0abe574dd105d734ccf30
hek316 <yhr05008@naver.com>
2023-09-10 18:58:50 +0900

    BaekJoon 11722 - [다이나믹 프로그래밍 1(연습)] 가장 긴 감소하는 부분 수열 - binarySearch 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
@@ -24,24 +25,49 @@ public class Main {
         int n = nextInt();
 
         int[] arr = new int[n];
-        int[] cnt = new int[n];
+        int[] b = new int[n];
 
         for (int i=0; i<n; i++) {
             arr[i] = nextInt();
         }
+        b[0] =  arr[0];
+        int len = 0;
 
-        int max = 1;
-        cnt[0] = 1;
         for (int i=1; i<n; i++) {
-            for(int j=0; j<i; j++){
-                if( arr[j] > arr[i]){
-                    cnt[i] = Math.max(cnt[j], cnt[i]);
-                }
-            }
-            cnt[i]++;
-            max = Math.max(max, cnt[i]);
+            len = binarySearch( arr[i], len, b);
+
         }
-        System.out.println(max);
+        System.out.println(len + 1);
+    }
+
+
+
+    public static int binarySearch(int target, int len, int[] b){
+        int start =0;
+
+        if(b[len] > target ){
+            len = len +1;
+            b[len] = target;
+            return  len;
+        }
+        int end = len;
+        int mid = 0;
+
+        while (start < end) {
+            mid = (start + end)/2;
+            if (target == b[mid]) {
+                break;
+            } else if (target > b[mid]) {
+                end = mid;
+            } else {
+                start = mid+1;
+                mid++;
+            }
+        }
+        if ( target > b[mid]) {
+            b[mid] = target;
+        }
+        return  len;
     }
 
 }
