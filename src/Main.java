9aeb5d892b7339335bb45cdb4c00ab75e4c26b4d
hek316 <hek3167@gmail.com>
2023-10-14 21:38:18 +0900

    BaekJoon 2309 - [500 - 브루트 포스] 일곱 난쟁이

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] rgb = new int[n][3];
-        int[][] dp = new int[n][3];
+        int n = 9;
+        int[] arr = new int[9];
+        int sum = 0;
 
-        String[] s ;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         for (int i = 0; i < n; i++) {
-            s = br.readLine().split(" ");
-            for(int j = 0; j < 3; j++){
-                rgb[i][j] = Integer.parseInt(s[j]);
-            }
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
 
-        int min = Integer.MAX_VALUE;
-        for(int k=0; k<3; k++){
-            for(int j=0; j<3; j++){
-                if(k==j){
-                    dp[1][j] = 2001;
-                }else {
-                    dp[1][j] = rgb[0][k]+ rgb[1][j];
+        Arrays.sort(arr);
+
+
+        Loop1 :
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (sum- arr[i] - arr[j] == 100) {
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j){
+                            continue;
+                        }
+                        System.out.println(arr[k]);
+                    }
+                    break Loop1;
                 }
             }
-            for (int i = 2; i < n; i++) {
-                dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
-                dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
-                dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
-            }
-            if (k==0) {
-                min = Math.min(dp[n-1][2],min);
-                min = Math.min(dp[n-1][1],min);
-            }else if (k==1) {
-                min = Math.min(dp[n-1][0],min);
-                min = Math.min(dp[n-1][2],min);
-            }else if (k==2) {
-                min = Math.min(dp[n-1][1],min);
-                min = Math.min(dp[n-1][0], min);
-            }
         }
-        System.out.println(min);
+
+
+
     }
+
 }
