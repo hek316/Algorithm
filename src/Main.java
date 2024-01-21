6aed737ecdd8ccc67465c67f331528e341645b90
hek316 <yhr05008@naver.com>
2024-01-21 22:20:26 +0900

    BaekJoon 3085 - [500 - 브루트 포스] 사탕 게임 (나머지 성질이용)

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        int E = 15;
+        int S = 28;
+        int M = 19;
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        char[][] arr = new char[n][n];
-        int max = 1;
-        for(int i=0; i<n; i++){
-            arr[i] = br.readLine().toCharArray();
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n-1; j++){
-                swapRight(arr, i, j);
-                max = Math.max(count(arr,i,j), max);
-                max = Math.max(count(arr,i,j+1), max);
-                swapRight(arr, i, j);
-            }
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n; j++){
-                swapDown(arr, i, j);
-                max = Math.max(count(arr,i,j), max);
-                max = Math.max(count(arr,i+1,j), max);
-                swapDown(arr, i, j);;
-            }
-        }
-        System.out.println(max);
-    }
-
-    static char[][] swapRight (char[][] arr, int i, int j){
-        char tmp = arr[i][j+1];
-        arr[i][j+1] =  arr[i][j];
-        arr[i][j] = tmp;
-        return arr;
-    }
-
-    static char[][] swapDown (char[][] arr, int i, int j){
-        char tmp = arr[i+1][j];
-        arr[i+1][j] =  arr[i][j];
-        arr[i][j] = tmp;
-        return arr;
-    }
-
-    static int count(char[][] arr,int i, int j){
-        int max = 1;
-        int cnt1 = 1;
-        for(int k=j-1; k>=0; k--){
-            if(arr[i][j] == arr[i][k]){
-                cnt1++;
-            }else {
+        int answer = 1;
+        int e , s , m ;
+        while (true) {
+            e = answer%E;
+            s = answer%S;
+            m = answer%M;
+            if (e == 0) e = E;
+            if (s == 0) s = S;
+            if (m == 0) m = M;
+            if (e == arr[0] &&  s == arr[1] && m == arr[2]) {
                 break;
-            }
-        }
-
-        for(int k=j+1; k<arr.length; k++){
-            if(arr[i][j] == arr[i][k]){
-                cnt1++;
             }else {
-                break;
+                answer++;
             }
         }
 
-        int cnt2 = 1;
-        for(int k=i-1; k>=0; k--){
-            if(arr[i][j] == arr[k][j]){
-                cnt2++;
-            }else {
-                break;
-            }
-        }
-
-        for(int k=i+1; k<arr.length; k++){
-            if(arr[i][j] == arr[k][j]){
-                cnt2++;
-            }else {
-                break;
-            }
-        }
-        max = Math.max(cnt1, cnt2);
-
-        return max;
+        System.out.println(answer);
     }
-
-
-
-
 }
