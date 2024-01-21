c1e47032c09d9674721086371c36136bc26756c8
hek316 <hek3167@gmail.com>
2024-01-21 11:24:18 +0900

    BaekJoon 3085 - [500 - 브루트 포스] 사탕 게임

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,92 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
-
-    public static void main(String[] args) throws IOException {
-
-        int n = 9;
-        int[] arr = new int[9];
-        int sum = 0;
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
+        int n = Integer.parseInt(br.readLine());
+        char[][] arr = new char[n][n];
+        int max = 1;
+        for(int i=0; i<n; i++){
+            arr[i] = br.readLine().toCharArray();
         }
-
-        Arrays.sort(arr);
-
-
-        Loop1 :
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (sum- arr[i] - arr[j] == 100) {
-                    for (int k = 0; k < n; k++) {
-                        if (k == i || k == j){
-                            continue;
-                        }
-                        System.out.println(arr[k]);
-                    }
-                    break Loop1;
-                }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-1; j++){
+                swapRight(arr, i, j);
+                max = Math.max(count(arr,i,j), max);
+                max = Math.max(count(arr,i,j+1), max);
+                swapRight(arr, i, j);
             }
         }
 
-
-
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n; j++){
+                swapDown(arr, i, j);
+                max = Math.max(count(arr,i,j), max);
+                max = Math.max(count(arr,i+1,j), max);
+                swapDown(arr, i, j);;
+            }
+        }
+        System.out.println(max);
     }
+
+    static char[][] swapRight (char[][] arr, int i, int j){
+        char tmp = arr[i][j+1];
+        arr[i][j+1] =  arr[i][j];
+        arr[i][j] = tmp;
+        return arr;
+    }
+
+    static char[][] swapDown (char[][] arr, int i, int j){
+        char tmp = arr[i+1][j];
+        arr[i+1][j] =  arr[i][j];
+        arr[i][j] = tmp;
+        return arr;
+    }
+
+    static int count(char[][] arr,int i, int j){
+        int max = 1;
+        int cnt1 = 1;
+        for(int k=j-1; k>=0; k--){
+            if(arr[i][j] == arr[i][k]){
+                cnt1++;
+            }else {
+                break;
+            }
+        }
+
+        for(int k=j+1; k<arr.length; k++){
+            if(arr[i][j] == arr[i][k]){
+                cnt1++;
+            }else {
+                break;
+            }
+        }
+
+        int cnt2 = 1;
+        for(int k=i-1; k>=0; k--){
+            if(arr[i][j] == arr[k][j]){
+                cnt2++;
+            }else {
+                break;
+            }
+        }
+
+        for(int k=i+1; k<arr.length; k++){
+            if(arr[i][j] == arr[k][j]){
+                cnt2++;
+            }else {
+                break;
+            }
+        }
+        max = Math.max(cnt1, cnt2);
+
+        return max;
+    }
+
+
+
 
 }
