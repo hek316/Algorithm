287b9dd12d4d1c80ae8e64b11eafc475f22d12f8
hek316 <yhr05008@naver.com>
2024-02-11 16:06:32 +0900

    BaekJoon 14500 - [500 - 브루트 포스] 테트로미노

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,118 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[] broken;
-
     public static void main(String[] args) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int k = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
-        int min = Math.abs(k-100);
-        if(n == 0){
-            System.out.println(Math.min(Integer.toString(k).length(), min));
-        }else {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            broken = new int[n];
-            for(int i=0; i<n; i++){
-                broken[i] = Integer.parseInt(st.nextToken());
-            }
-            int tmp = k;
+        int column = read();
+        int row = read();
 
-            while(tmp <= 1000000){
-                int len =isPossible(tmp);
-                if(len != 0){
-                    min = Math.min(min,len + Math.abs(tmp-k));
-                    break;
-                }else {
-                    tmp++;
-                }
+        int[][] arr = new int[column][row];
+        for(int i = 0; i < column; i++){
+            for(int j = 0; j < row; j++){
+                arr[i][j] = read();
             }
-
-            int tmp2 = k;
-            while(tmp2 >= 0){
-                int len =isPossible(tmp2);
-                if(len != 0){
-                    min = Math.min(min,len + Math.abs(tmp2-k));
-                    break;
-                }else {
-                    tmp2--;
-                }
-            }
-
-            System.out.println(min);
         }
+        int max = threeMax(arr);
+        max = Math.max(twoMax(arr),max);
+        System.out.println(max);
+
     }
 
-    private static int isPossible(int k){
-        if(k == 0){
-            for(int i=0; i <broken.length; i++){
-                if(broken[i] == k){
-                    return 0;
-                }
-            }
-            return 1;
-        }
-        int len = 0;
-        while (k > 0){
-            for(int i=0; i <broken.length; i++){
-                if(broken[i] == k % 10){
-                    return 0;
-                }
-            }
-            len++;
-            k /= 10;
-        }
-        return len;
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while((c=System.in.read())>32) n = (n<<3) + (n<<1) + (c&15);
+        return n;
     }
+
+    static private  int threeMax(int[][] arr){
+        int max = 0;
+
+        // 세로로 3연속
+        for(int i=0; i+2 < arr.length; i++){
+            for(int j=0; j <arr[0].length; j++){
+                int sum = arr[i][j] + arr[i+1][j] + arr[i+2][j];
+                if(j+1 < arr[0].length){
+                    int tmp1 = Math.max(arr[i][j+1], arr[i+1][j+1]);
+                    tmp1 = Math.max(tmp1, arr[i+2][j+1]);
+                    max = Math.max(tmp1+ sum, max);
+                }
+
+                if(j- 1 >= 0){
+                    int tmp1 = Math.max(arr[i][j-1], arr[i+1][j-1]);
+                    tmp1 = Math.max(tmp1, arr[i+2][j-1]);
+                    max = Math.max(tmp1+sum, max);
+                }
+
+                // 연속 4줄
+                if(i+ 3 < arr.length){
+                    max = Math.max(arr[i+3][j]+sum, max);
+                }
+            }
+        }
+
+
+        // 가로로 3연속
+        for(int i=0; i< arr.length; i++){
+            for(int j=0; j+2 <arr[0].length; j++){
+                int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                if(i+1 < arr.length){
+                    int tmp1 = Math.max(arr[i+1][j], arr[i+1][j+1]);
+                    tmp1 = Math.max(tmp1, arr[i+1][j+2]);
+                    max = Math.max(tmp1+ sum, max);
+                }
+
+                if(i- 1 >= 0){
+                    int tmp1 = Math.max(arr[i-1][j], arr[i-1][j+1]);
+                    tmp1 = Math.max(tmp1, arr[i-1][j+2]);
+                    max = Math.max(tmp1+sum, max);
+                }
+                // 연속 4줄
+                if(j+ 3 < arr[0].length){
+                    max = Math.max(arr[i][j+3]+sum, max);
+                }
+            }
+        }
+
+        return max;
+    }
+
+    static int twoMax(int[][] arr) {
+        int max = 0;
+        //세로 연속2
+        for (int i = 0; i+1 < arr.length; i++) {
+            for(int j= 0; j < arr[0].length; j++){
+                int sum = arr[i][j] + arr[i+1][j];
+                if(j+1 < arr[0].length){
+                    int tmp = arr[i][j+1] + arr[i+1][j+1];
+                    if(i+2 < arr.length){
+                        tmp = Math.max(tmp, arr[i+1][j+1] + arr[i+2][j+1]);
+                    }
+                    if(i-1 >= 0) {
+                        tmp = Math.max(tmp, arr[i-1][j+1] + arr[i][j+1]);
+                    }
+                    max = Math.max(sum+ tmp, max);
+                }
+            }
+        }
+        // 가로연속
+        for (int i = 0; i< arr.length-1; i++) {
+            for(int j= 0; j+1 < arr[0].length; j++) {
+                int sum = arr[i][j] +  arr[i][j+1];
+                if(i + 1 < arr.length){
+                    int tmp = 0;
+                    if( j-1 >= 0 ){
+                        tmp  = arr[i+1][j-1] +arr[i+1][j];
+                    }
+                    if( j+2 < arr[0].length){
+                        tmp = Math.max(arr[i+1][j+1] +arr[i+1][j+2],tmp);
+                    }
+                    max  =Math.max(sum+ tmp, max);
+                }
+            }
+        }
+        return max;
+    }
+
 
 }
