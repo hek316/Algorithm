7da04e8ea4831ebc9e614eb04f39ae28a228903d
hek316 <yhr05008@naver.com>
2024-03-01 20:49:43 +0900

    BaekJoon 1748 - [500 - 브루트 포스] 수 이어 쓰기 1 풀이방법1 (경우의 수 곱하기)

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,31 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        long sum = 0;
+        int n = Integer.parseInt(s);
 
-    public static void main(String[] args) throws Exception {
-
-
-        int n = read();
-        int[][] arr = new int[n][4];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 4; j++) {
-                arr[i][j] = read();
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-
-
-        for(int i=0; i<arr.length; i++){
-
-            sb.append(solveYear(arr[i][0], arr[i][1], arr[i][2], arr[i][3])).append("\n");
-        }
-        System.out.println(sb);
-    }
-    static int solveYear(int n , int m, int x, int y){
-        int i = 0;
-        int lcm = n*m/gcd(m,n);
-        int year = n*i+x;
-        while(year <= lcm){
-            int mod = year%m;
-            if(mod == 0){
-                mod = m;
-            }
-            if(mod==y){
-                return year;
-            }
+        int i= 1;
+        long k = (long) Math.pow(10, 0);
+        long mok = n/10;
+        while( (mok) >0 ){
+            sum = sum + 9*(long) Math.pow(10, i-1)*i;
             i++;
-            year = n*i+x;
-        }
-        return -1;
-    }
-
-    static int gcd(int a, int b){
-        if(a < b){
-            int tmp = a;
-            a = b;
-            b = tmp;
+            k = (long) Math.pow(10, i);
+            mok = n/k;
         }
 
-        int mod = a%b;
-        if (mod != 0){
-            return gcd(b,mod);
-        } else {
-            return b;
-        }
+        long tmp = n- (long)Math.pow(10, i-1)+1;
+        sum += tmp*s.length();
+
+
+        System.out.println(sum);
     }
 
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
-    }
+
 
 }
