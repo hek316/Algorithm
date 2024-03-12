10258f59187091cc125e1f3ad855bbe01994c2b1
hek316 <yhr05008@naver.com>
2024-03-12 22:16:31 +0900

    BaekJoon 15656 - [510 - 브루트 포스 (N과 M)] N과 M (7)

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,37 @@ import java.util.StringTokenizer;
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int[] result;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s =br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
         int m = Integer.parseInt(s[1]);
 
-        int[] arr = new int[n];
-        int[] result = new int[n];
+        arr = new int[n];
+        result = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<arr.length; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        dfs(m, arr, result, 0, 0);
+        dfs(m, 0, 0);
         System.out.println(sb);
     }
 
-    static private void dfs(int m, int[] arr, int[] result, int idx, int start){
-        if( m <= idx ){
+    static private void dfs(int m, int idx, int start){
+        if( m == idx ){
             for(int i=0; i<m; i++){
                 sb.append(result[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i= start; i< arr.length; i++){
+        for(int i= 0; i< arr.length; i++){
             result[idx] = arr[i];
-            dfs(m, arr, result, idx+1, i+1);
+            dfs(m,idx+1, i);
         }
     }
 }
