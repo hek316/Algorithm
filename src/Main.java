7261193994a4916ec57efe70f1b538269cef0bff
hek316 <yhr05008@naver.com>
2024-03-23 15:40:41 +0900

    BaekJoon 10972 - [520 - 브루트 포스 - 순열] 다음 순열 - 풀이방법2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int[] arr;
-    static boolean[] visit;
-    static int[] result;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
 
         arr = new int[n];
-        result = new int[n];
-        visit = new boolean[n+1];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<arr.length; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-        }
-        for(int i=0; i<=n; i++){
-            visit[i] = true;
         }
 
         String s  = nextArr(arr, n);
@@ -29,49 +22,43 @@ public class Main {
     }
 
     static private String nextArr(int[] arr, int n){
-        int result = -1;
-        int tmp = arr[n-1];
-        visit[arr[n-1]] = false;
-        for(int i=n-2; i>=0; i--){
-            visit[arr[i]] = false;
-            if(tmp < arr[i]){
-                tmp = arr[i];
-            }else {
-                if(arr[i] !=n){
-                    int k = arr[i] +1;
-                    while(k <= n){
-                        if(visit[k] == false){
-                            arr[i] = k;
-                            visit[k] = true;
-                            break;
-                        }
-                        k++;
-                    }
-                }
-                result = i;
+        int idx = n-1;
+        // 마지막 순열일 경우 내림차순
+        while(idx>=1 && arr[idx-1] > arr[idx] ){
+            idx--;
+        }
+        StringBuilder sb = new StringBuilder();
+        if(idx == 0){
+            sb.append("-1");
+            return sb.toString();
+        }
+
+        int j = idx-1;
+        for(int i = n-1; i>= idx; i--){
+            if(arr[j] < arr[i]){
+                swap(i, j, arr);
                 break;
             }
         }
-        if(result != -1){
-            for(int i=result+1; i<=n-1; i++){
-                for(int j=1; j<=n; j++){
-                    if(visit[j] == false){
-                        arr[i] = j;
-                        visit[j] = true;
-                        i++;
-                    }
-                }
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        if(result != -1){
-            for(int i=0; i<= arr.length-1; i++){
-                sb.append(arr[i]).append(" ");
-            }
-        } else {
-            sb.append("-1");
+
+        j = n-1;
+        while(idx < j){
+            swap(idx, j, arr);
+            idx++;
+            j--;
         }
 
+
+        for(int i=0; i<= arr.length-1; i++){
+            sb.append(arr[i]).append(" ");
+        }
         return sb.toString();
     }
+
+    private static void swap(int i, int j, int[] arr){
+        int tmp =arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
