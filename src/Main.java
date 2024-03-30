672895cfe51884533a6a89e7e76c755976970797
hek316 <yhr05008@naver.com>
2024-03-30 23:45:55 +0900

    BaekJoon 10819 - [520 - 브루트 포스 - 순열] 차이를 최대로

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] arr;
 
     static boolean[] visit;
-
-    static StringBuilder sb = new StringBuilder();
-
+    static int MAX ;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        br.close();
 
-        arr = new int[n];
-        visit = new boolean[n+1];
-        nextArr(0, n);
-        System.out.println(sb);
+        int[] arr = new int[n];
+        int[] result = new int[n];
+        visit = new boolean[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        nextArr(0, n, result, arr);
+        System.out.println(MAX);
+
     }
 
-    static private void nextArr(int idx, int n){
-        if(idx == n){
-            for(int i=0; i<n; i++){
-                sb.append(arr[i]).append(" ");
+
+    public static void nextArr(int dept, int n, int[] result, int[] arr){
+        if(dept == n){
+            int tmp = 0;
+            for(int i=0; i<n-1; i++){
+                tmp += Math.abs(result[i + 1] - result[i]);
             }
-            sb.append("\n");
+            if(tmp>MAX){
+                MAX = tmp;
+            }
             return;
         }
-        for(int i = 1; i <= n; i++){
+
+        for(int i=0; i<n; i++){
             if(visit[i] == false){
-                arr[idx] = i;
                 visit[i] = true;
-                nextArr(idx+1, n);
+                result[dept] = arr[i];
+                nextArr(dept+1, n, result, arr);
                 visit[i] = false;
             }
         }
+
     }
+
 }
