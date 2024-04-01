77d421f6b64e58edb8a22ceb8960d24fbfefe613
hek316 <yhr05008@naver.com>
2024-04-01 22:38:40 +0900

    BaekJoon 10971 - [520 - 브루트 포스 - 순열] 외판원 순회 2

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,63 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-
+    static int[][] arr;
     static boolean[] visit;
-    static int MAX ;
+
+    static int MIN;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[n];
-        int[] result = new int[n];
+        arr = new int[n][n];
         visit = new boolean[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        br.close();
-        for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        MIN = Integer.MAX_VALUE;
+
+
+        StringTokenizer st ;
+        for(int i=0; i<arr.length; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<arr.length; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        nextArr(0, n, result, arr);
-        System.out.println(MAX);
+
+        for(int j=0; j<arr.length; j++){
+            if(arr[0][j]== 0){
+                continue;
+            }
+            visit[0] = true;
+            visit[j] = true;
+            nextArr(1,n, j, arr[0][j]);
+            visit[j] = false;
+        }
+        System.out.println(MIN);
 
     }
 
-
-    public static void nextArr(int dept, int n, int[] result, int[] arr){
-        if(dept == n){
-            int tmp = 0;
-            for(int i=0; i<n-1; i++){
-                tmp += Math.abs(result[i + 1] - result[i]);
+    public static void nextArr(int idx, int n, int now, int cost){
+        if(idx == n-1){
+            if(arr[now][0] == 0){
+                return;
             }
-            if(tmp>MAX){
-                MAX = tmp;
-            }
+            // 첫 행선지로 돌아가기
+            cost += arr[now][0];
+            MIN = Math.min(cost, MIN);
             return;
         }
 
-        for(int i=0; i<n; i++){
-            if(visit[i] == false){
-                visit[i] = true;
-                result[dept] = arr[i];
-                nextArr(dept+1, n, result, arr);
-                visit[i] = false;
+        for(int j=1; j<n; j++){
+            // 다음에 갈 행선지
+            if(visit[j] == false && arr[now][j] != 0){
+                visit[j] = true;
+                nextArr(idx+1,n,j, cost+  arr[now][j]);
+                visit[j] = false;
             }
         }
 
     }
+
+
 
 }
