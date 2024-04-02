c4e014d00fec4a846428c2b30e94b38f3948c0cb
hek316 <yhr05008@naver.com>
2024-04-02 22:21:50 +0900

    BaekJoon 6603 - [520 - 브루트 포스 - 순열] 로또

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,46 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static boolean[] visit;
 
-    static int MIN;
+    public static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        visit = new boolean[n];
-        MIN = Integer.MAX_VALUE;
+        String input = br.readLine();
+        String[] s1 = input.split(" ");
 
 
-        StringTokenizer st ;
-        for(int i=0; i<arr.length; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<arr.length; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        while(!s1[0].equals("0")){
+            int n = Integer.parseInt(s1[0]);
+            int[] arr = new int[n];
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(s1[i+1]);
             }
-        }
+            input = br.readLine();
+            s1 = input.split(" ");
+            s1[0] = input.split(" ")[0];
 
-        for(int j=0; j<arr.length; j++){
-            if(arr[0][j]== 0){
-                continue;
-            }
-            visit[0] = true;
-            visit[j] = true;
-            nextArr(1,n, j, arr[0][j]);
-            visit[j] = false;
+            int[] result = new int[6];
+            nextArr(0, result, arr, 0);
+            sb.append("\n");
         }
-        System.out.println(MIN);
+        System.out.println(sb);
 
     }
-
-    public static void nextArr(int idx, int n, int now, int cost){
-        if(idx == n-1){
-            if(arr[now][0] == 0){
-                return;
+    public static void nextArr(int idx, int[] result,int[] arr, int start){
+        if(idx == 6){
+            for(int i=0; i<6; i++){
+                sb.append(result[i]).append(" ");
             }
-            // 첫 행선지로 돌아가기
-            cost += arr[now][0];
-            MIN = Math.min(cost, MIN);
+            sb.append("\n");
             return;
         }
 
-        for(int j=1; j<n; j++){
-            // 다음에 갈 행선지
-            if(visit[j] == false && arr[now][j] != 0){
-                visit[j] = true;
-                nextArr(idx+1,n,j, cost+  arr[now][j]);
-                visit[j] = false;
-            }
+        for(int i=start; i<arr.length; i++){
+            result[idx] = arr[i];
+            nextArr(idx+1, result, arr, i+1);
         }
-
     }
-
-
 
 }
