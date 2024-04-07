8ce86e7e9befcdcb8b6e97baf91073eb72ea9589
hek316 <yhr05008@naver.com>
2024-04-07 12:54:05 +0900

    BaekJoon 9095 - [530 - 브루트 포스 - 재귀] 1, 2, 3 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
-    public static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        String[] s1 = input.split(" ");
 
-
-        while(!s1[0].equals("0")){
-            int n = Integer.parseInt(s1[0]);
-            int[] arr = new int[n];
-            for(int i=0; i<n; i++){
-                arr[i] = Integer.parseInt(s1[i+1]);
-            }
-            input = br.readLine();
-            s1 = input.split(" ");
-            s1[0] = input.split(" ")[0];
-
-            int[] result = new int[6];
-            nextArr(0, result, arr, 0);
-            sb.append("\n");
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[n];
+        int max = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(arr[i], max);
         }
+        br.close();
+        int[] memo = new int[max+1];
+        if(max>3){
+            memo[1] = 1;
+            memo[2] = 2;
+            memo[3] = 4;
+            getOneTwoThreePlus(max, memo);
+        }
+
+        for(int i=0; i<n; i++){
+            sb.append(getOneTwoThreePlus(arr[i], memo)).append("\n");
+        }
+
         System.out.println(sb);
 
     }
-    public static void nextArr(int idx, int[] result,int[] arr, int start){
-        if(idx == 6){
-            for(int i=0; i<6; i++){
-                sb.append(result[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
 
-        for(int i=start; i<arr.length; i++){
-            result[idx] = arr[i];
-            nextArr(idx+1, result, arr, i+1);
+    public static int getOneTwoThreePlus(int i, int[] memo){
+        if(i< 2){
+            return i;
+        }
+        if(i == 3){
+            return 4;
+        }
+        if(memo[i] != 0){
+            return memo[i];
+        }else {
+            return getOneTwoThreePlus(i-1, memo)+getOneTwoThreePlus(i-2, memo) +getOneTwoThreePlus(i-3, memo);
         }
     }
+
+
 
 }
