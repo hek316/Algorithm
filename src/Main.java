cd6c3e60a1f00e25582beae2fec77568afb14913
hek316 <yhr05008@naver.com>
2024-05-04 02:12:20 +0900

    BaekJoon 14889 - [530 - 브루트 포스 - 재귀] 스타트와 링크 풀이3

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static int MIN;
-    static int[][] arr;
-
-    static int[] result;
-
-    static int[] result2;
+    static int[] row;
+    static int[] column;
+    static int totalSum = 0;
 
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        result = new int[N];
-        result2 = new int[N];
+        row = new int[N];
+        column = new int[N];
         MIN = Integer.MAX_VALUE;
 
         for(int i=0; i< N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                row[i] += num;
+                column[j] += num;
+                totalSum += num;
             }
         }
-        // 0번째팀을 가진 경우만 비교해보아도 모든 경우의 수 파악가능
-        result[0] = 0;
-        go(1,1);
+        go(0,totalSum,0);
         System.out.println(MIN);
 
     }
 
-    public static void go(int idx, int start){
-        if(idx == N/2){
-
-            boolean[] resultTmp = new boolean[N];
-            for(int i=0; i<N/2; i++){
-                resultTmp[result[i]] = true;
-            }
-            int j =0;
-            for(int i=0; i<N; i++){
-                if(resultTmp[i] == false){
-                    result2[j] = i;
-                    j++;
-                }
-            }
-            int sum1 = solveLevel1(result, 0);
-            int sum2 = solveLevel1(result2, 0);
-            int tmp = Math.abs(sum2- sum1);
-            MIN = Math.min(MIN, tmp);
+    public static void go(int idx, int totalSum, int cnt){
+        if(idx == N){
             return;
         }
-
-        for(int i=start; i<N; i++){
-            result[idx] = i;
-
-            go(idx+1,  i+1);
+        if(cnt == N/2){
+            // 두 팀의 능력치 차
+            int absSum =Math.abs(totalSum);
+            MIN = Math.min(absSum, MIN);
+            return;
         }
-    }
+        // 선택한 경우
+        go(idx+1, totalSum - row[idx] - column[idx], cnt+1);
+        // 선택안한 경우
+        go(idx+1, totalSum, cnt);
 
-    public static int solveLevel1(int[] result2, int sum){
 
-        for(int i=0; i<N/2; i++){
-            for(int j=i+1; j<N/2; j++){
-                sum= sum + arr[result2[i]][result2[j]] + arr[result2[j]][result2[i]] ;
-            }
-        }
-        return sum;
     }
 
 }
