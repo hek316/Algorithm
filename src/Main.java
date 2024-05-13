3f30f4a30c567a9086ff80f61d3f650539dc412f
hek316 <yhr05008@naver.com>
2024-05-13 23:25:25 +0900

    BaekJoon 1248 - [530 - 브루트 포스 - 재귀] Guess

--- src/Main.java
+++ src/Main.java
@@ -1,91 +1,83 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+
 
 public class Main {
-    static int N;
-    static char[] arr;
-
-    static int[] result;
-
-    static boolean[] isvisit;
-
-    static StringBuilder sb = new StringBuilder();
-
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new char[N];
-        isvisit = new boolean[10];
-        result = new int[N+1];
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        int[] answer = new int[n];
 
         String s = br.readLine();
-        for(int i =0; i<N; i++){
-            arr[i] = s.charAt(2*i);
-        }
 
-        solveMax(arr, isvisit, 0, result);
-        Arrays.fill(isvisit, false);
-        solve(arr, isvisit, 0, result);
-        System.out.println(sb);
-    }
-
-    public static boolean solve(char[] arr, boolean[] isvisit, int idx, int[] result){
-        if(idx ==  N+1){
-            for(int i : result){
-                sb.append(i);
-            }
-            sb.append("\n");
-            return true;
-        }
-
-        for(int i=0; i<10; i++){
-            if(isvisit[i] == true) continue;
-            if(idx == 0 || isVaild(result[idx-1], i, arr[idx-1])){
-                result[idx] = i;
-                isvisit[i] = true;
-                if(solve(arr, isvisit, idx+1, result)){
-                    return true;
+        int k = 0;
+        for(int i=0; i< n; i++){
+            for(int j =i; j< n; j++){
+                if(s.charAt(k) == '+'){
+                    arr[i][j] = 1;
+                }else if(s.charAt(k) == '-'){
+                    arr[i][j] = -1;
+                }else if(s.charAt(k) == '0') {
+                    arr[i][j] = 0;
                 }
-                isvisit[i] = false;
+                k++;
             }
         }
-        return false;
+        getAnswer(answer, n, 0, arr);
+
     }
 
-    public static boolean solveMax(char[] arr, boolean[] isvisit, int idx, int[] result){
-        if(idx ==  N+1){
-            for(int i : result){
-                sb.append(i);
+    public static boolean getAnswer(int[] answer, int n, int idx, int[][] arr){
+        if(idx == n){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< n; i++){
+                sb.append(answer[i]).append(" ");
             }
-            sb.append("\n");
+            System.out.println(sb);
             return true;
+
         }
 
-        for(int i=9; i>=0; i--){
-            if(isvisit[i] == true) continue;
-            if(idx == 0 || isVaild(result[idx-1], i, arr[idx-1])){
-                result[idx] = i;
-                isvisit[i] = true;
-                if(solveMax(arr, isvisit, idx+1, result)){
-                    return true;
-                }
-                isvisit[i] = false;
-            }
-        }
-        return false;
-    }
-    public static boolean isVaild(int preNum, int num, char inequality ){
-        if(inequality == '<'){
-            if(preNum < num){
+        if(arr[idx][idx] == '0'){
+            answer[idx] = 0;
+            if(getAnswer(answer, n, idx+1, arr)){
                 return true;
             }
-        }else{
-            if(preNum > num){
-                return true;
+        } else {
+            for(int j =1; j<= 10; j++){
+                answer[idx] = j*arr[idx][idx];
+                if(isValid(answer, idx, arr) && getAnswer(answer, n, idx+1, arr) ){
+                    return true;
+                }
             }
         }
         return false;
     }
+
+    public static boolean isValid(int[] answer, int k, int[][] arr){
+        for(int i=0; i<=k; i++){
+            int sum = 0;
+            for(int j=i; j<=k; j++){
+                sum += answer[j];
+            }
+            if(sum > 0){
+                if(arr[i][k] != 1 ){
+                    return false;
+                }
+            } else if( sum < 0){
+                if(arr[i][k] != -1 ){
+                    return false;
+                }
+            } else if( sum == 0){
+                if(arr[i][k] != 0 ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
