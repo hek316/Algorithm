b304bdd626513b43b3f7ae6dbe73fada997055d8
hek316 <hek3167@gmail.com>
2024-05-21 06:59:21 +0900

    BaekJoon 1182 - [540 - 브루트 포스 - dfs] 부분수열의 합

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
-        int[] answer = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
 
-        String s = br.readLine();
+        int[] arr = new int[N];
+        int sum = Integer.parseInt(st.nextToken());
 
-        int k = 0;
-        for(int i=0; i< n; i++){
-            for(int j =i; j< n; j++){
-                if(s.charAt(k) == '+'){
-                    arr[i][j] = 1;
-                }else if(s.charAt(k) == '-'){
-                    arr[i][j] = -1;
-                }else if(s.charAt(k) == '0') {
-                    arr[i][j] = 0;
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        int cnt = 0;
+        for (int i = 1; i < (1 << N); i++) {
+            int total = 0;
+            for (int j = 0; j < N; j++) {
+                if ((i & (1 << j)) != 0) {
+                    total+= arr[j];
                 }
-                k++;
+            }
+            if(total == sum){
+                cnt++;
             }
         }
-        getAnswer(answer, n, 0, arr);
-
+        System.out.println(cnt);
     }
-
-    public static boolean getAnswer(int[] answer, int n, int idx, int[][] arr){
-        if(idx == n){
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< n; i++){
-                sb.append(answer[i]).append(" ");
-            }
-            System.out.println(sb);
-            return true;
-
-        }
-
-        if(arr[idx][idx] == '0'){
-            answer[idx] = 0;
-            if(getAnswer(answer, n, idx+1, arr)){
-                return true;
-            }
-        } else {
-            for(int j =1; j<= 10; j++){
-                answer[idx] = j*arr[idx][idx];
-                if(isValid(answer, idx, arr) && getAnswer(answer, n, idx+1, arr) ){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public static boolean isValid(int[] answer, int k, int[][] arr){
-        for(int i=0; i<=k; i++){
-            int sum = 0;
-            for(int j=i; j<=k; j++){
-                sum += answer[j];
-            }
-            if(sum > 0){
-                if(arr[i][k] != 1 ){
-                    return false;
-                }
-            } else if( sum < 0){
-                if(arr[i][k] != -1 ){
-                    return false;
-                }
-            } else if( sum == 0){
-                if(arr[i][k] != 0 ){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
 }
