723cf8626b32bbb1e093fb1493ad062e16375b26
hek316 <hek3167@gmail.com>
2024-05-21 09:24:16 +0900

    BaekJoon 14889 - [540 - 브루트 포스 - 비트마스크] 스타트와 링크

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-
-    static int sum;
-
-    static int cnt;
-
-    static int[] arr;
+    static int[][] arr;
+    static int MIN;
 
 
     public static void main(String[] args) throws Exception {
@@ -18,28 +14,61 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
-        sum = Integer.parseInt(st.nextToken());
+        arr = new int[N][N];
+        MIN = Integer.MAX_VALUE;
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        find(0, 0);
+        for(int i=1; i< (1<<N) ; i++){
+            int cnt = 0;
 
-        if(sum == 0) cnt--;
-        System.out.println(cnt);
+            for(int j=0; j<N; j++){
+                if((i&(1<<j)) != 0){
+                    cnt++;
+                }
+            }
+            if(cnt == N/2){
+                int idx = 0;
+                int[] result = new int[N/2];
+                int[] result2 = new int[N/2];
+                for(int j=0; j<N; j++){
+                    if((i&(1<<j)) != 0){
+                        result[idx] = j;
+                        idx++;
+                    }
+                }
+                idx = 0;
+                for(int j=0; j<N; j++){
+                    if((i&(1<<j)) == 0){
+                        result2[idx] = j;
+                        idx++;
+                    }
+                }
+                int sum1 = solve(result);
+                int sum2 = solve(result2);
+                int tmp = Math.abs(sum2-sum1);
+                MIN = Math.min(tmp, MIN);
+
+            }
+        }
+        System.out.println(MIN);
+
     }
 
-    public static void find(int idx,int total){
-        if(idx == N){
-            if(total == sum){
-                cnt++;
+    public static int solve(int[] result){
+        int sum = 0;
+        for(int i=0; i<result.length; i++){
+            for(int j=i+1; j< result.length; j++){
+                int i2 = result[i];
+                int j2 = result[j];
+                sum = sum + arr[i2][j2] + arr[j2][i2];
             }
-            return;
         }
-        find(idx+1, total + arr[idx]);
-        find(idx+1, total);
+        return sum;
     }
 }
