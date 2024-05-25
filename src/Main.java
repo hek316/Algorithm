161a017a6df3a330565f1ba684499a169a8e4196
hek316 <yhr05008@naver.com>
2024-05-25 21:59:09 +0900

    BaekJoon 14391 - [540 - 브루트 포스 - 비트마스크] 종이 조각

--- src/Main.java
+++ src/Main.java
@@ -4,71 +4,68 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
+    static int M;
     static int[][] arr;
-    static int MIN;
+
+    static int MAX;
 
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][N];
-        MIN = Integer.MAX_VALUE;
+        int nm = N*M;
 
+        arr = new int[N][M];
+
+        int k = 0;
         for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            String s = br.readLine();
+            for(int j=0; j<M; j++){
+                arr[i][j] = s.charAt(j) - '0';
             }
         }
 
-        for(int i=1; i< (1<<N) ; i++){
-            int cnt = 0;
-
+        for(int i=0; i< (1 << nm ); i++){
+            // 0 : - ,  1 : |
+            int sum = 0;
             for(int j=0; j<N; j++){
-                if((i&(1<<j)) != 0){
-                    cnt++;
+                int cur = 0;
+                for (int m = 0; m < M; m++) {
+                    int tmp = j*M + m;
+                    if((i & (1 << tmp)) == 0){
+                        cur = cur*10 + arr[j][m];
+                    } else {
+                        sum+= cur;
+                        cur = 0;
+                    }
                 }
+                sum+= cur;
             }
-            if(cnt == N/2){
-                int idx = 0;
-                int[] result = new int[N/2];
-                int[] result2 = new int[N/2];
-                for(int j=0; j<N; j++){
-                    if((i&(1<<j)) != 0){
-                        result[idx] = j;
-                        idx++;
-                    }
-                }
-                idx = 0;
-                for(int j=0; j<N; j++){
-                    if((i&(1<<j)) == 0){
-                        result2[idx] = j;
-                        idx++;
-                    }
-                }
-                int sum1 = solve(result);
-                int sum2 = solve(result2);
-                int tmp = Math.abs(sum2-sum1);
-                MIN = Math.min(tmp, MIN);
 
+
+
+            for (int m = 0; m < M; m++) {
+                int cur = 0;
+                for(int j=0; j<N; j++){
+                    int tmp = j*M + m;
+                    if((i & (1 << tmp)) != 0){
+                        cur = cur*10 + arr[j][m];
+                    } else {
+                        sum+= cur;
+                        cur = 0;
+                    }
+                }
+                sum+= cur;
             }
+
+            MAX = Math.max(MAX, sum);
         }
-        System.out.println(MIN);
+
+        System.out.println(MAX);
 
     }
 
-    public static int solve(int[] result){
-        int sum = 0;
-        for(int i=0; i<result.length; i++){
-            for(int j=i+1; j< result.length; j++){
-                int i2 = result[i];
-                int j2 = result[j];
-                sum = sum + arr[i2][j2] + arr[j2][i2];
-            }
-        }
-        return sum;
-    }
 }
