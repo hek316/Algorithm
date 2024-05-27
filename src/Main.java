aa0e2e75a674663981e49f102de6399493ed7fe5
hek316 <hek3167@gmail.com>
2024-05-27 15:09:39 +0900

    BaekJoon 13023 - [600 - 그래프 1]  ABCDE 풀이방식2 (dfs)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 class Edge{
@@ -17,64 +16,52 @@ class Edge{
 
 public class Main {
 
-
+    static boolean[] visited;
+    static int answer;
+    static ArrayList<Integer>[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        //인접행렬
-        boolean[][] a = new boolean[n][n];
+
+        visited = new boolean[n];
         //인접리스트
-        ArrayList<Integer> g[] = (ArrayList<Integer>[]) new ArrayList[n];
-        // 간선리스트
-        ArrayList<Edge> edges = new ArrayList<Edge>();
+        list = (ArrayList<Integer>[])new ArrayList[n];
 
         for(int i=0; i<n; i++){
-            g[i] = new ArrayList<Integer>();
+            list[i] = new ArrayList<Integer>();
         }
+
 
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            a[from][to] = a[to][from] = true;
-            g[from].add(to);
-            g[to].add(from);
-            edges.add(new Edge(from, to));
-            edges.add(new Edge(to, from));
+            list[from].add(to);
+            list[to].add(from);
         }
 
-        m *= 2;
+        for(int i=0; i<n; i++){
+            if(answer != 1) dfs(1,i);
+        }
+        System.out.println(answer);
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<m; j++){
-                // A - > B
-                int A = edges.get(i).from;
-                int B = edges.get(i).to;
+    }
 
-                // C - > D
-                int C = edges.get(j).from;
-                int D = edges.get(j).to;
-
-                if(A == B || A == C || A == D || B == C || B == D || C == D ){
-                    continue;
-                }
-
-                if(a[B][C] == false){
-                    continue;
-                }
-                // D - > E
-                for(int E : g[D]){
-                    if(A == E || B == E || C == E || D == E){
-                        continue;
-                    }
-                    System.out.println(1);
-                    System.exit(0);
-                }
+    public static void dfs(int dept, int start){
+        if(dept == 5){
+            answer = 1;
+            return;
+        }
+        visited[start] = true;
+        for(int to :  list[start]){
+            if(visited[to] == false){
+                dfs(dept+1, to);
             }
         }
-        System.out.println(0);
+        visited[start] = false;
+
     }
 }
