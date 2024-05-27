304624c73f5284b9f4bae3ccaad10413be336f59
hek316 <hek3167@gmail.com>
2024-05-27 15:08:39 +0900

    BaekJoon 13023 - [600 - 그래프 1]  ABCDE 풀이방식1 (그래프)

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,80 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int N;
-    static int M;
-    static int[][] arr;
+class Edge{
+    int from, to;
 
-    static int MAX;
-
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        int nm = N*M;
-
-        arr = new int[N][M];
-
-        int k = 0;
-        for(int i=0; i<N; i++){
-            String s = br.readLine();
-            for(int j=0; j<M; j++){
-                arr[i][j] = s.charAt(j) - '0';
-            }
-        }
-
-        for(int i=0; i< (1 << nm ); i++){
-            // 0 : - ,  1 : |
-            int sum = 0;
-            for(int j=0; j<N; j++){
-                int cur = 0;
-                for (int m = 0; m < M; m++) {
-                    int tmp = j*M + m;
-                    if((i & (1 << tmp)) == 0){
-                        cur = cur*10 + arr[j][m];
-                    } else {
-                        sum+= cur;
-                        cur = 0;
-                    }
-                }
-                sum+= cur;
-            }
-
-
-
-            for (int m = 0; m < M; m++) {
-                int cur = 0;
-                for(int j=0; j<N; j++){
-                    int tmp = j*M + m;
-                    if((i & (1 << tmp)) != 0){
-                        cur = cur*10 + arr[j][m];
-                    } else {
-                        sum+= cur;
-                        cur = 0;
-                    }
-                }
-                sum+= cur;
-            }
-
-            MAX = Math.max(MAX, sum);
-        }
-
-        System.out.println(MAX);
-
+    Edge(int from, int to){
+        this.from = from;
+        this.to = to;
     }
 
+}
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        //인접행렬
+        boolean[][] a = new boolean[n][n];
+        //인접리스트
+        ArrayList<Integer> g[] = (ArrayList<Integer>[]) new ArrayList[n];
+        // 간선리스트
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+
+        for(int i=0; i<n; i++){
+            g[i] = new ArrayList<Integer>();
+        }
+
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            a[from][to] = a[to][from] = true;
+            g[from].add(to);
+            g[to].add(from);
+            edges.add(new Edge(from, to));
+            edges.add(new Edge(to, from));
+        }
+
+        m *= 2;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<m; j++){
+                // A - > B
+                int A = edges.get(i).from;
+                int B = edges.get(i).to;
+
+                // C - > D
+                int C = edges.get(j).from;
+                int D = edges.get(j).to;
+
+                if(A == B || A == C || A == D || B == C || B == D || C == D ){
+                    continue;
+                }
+
+                if(a[B][C] == false){
+                    continue;
+                }
+                // D - > E
+                for(int E : g[D]){
+                    if(A == E || B == E || C == E || D == E){
+                        continue;
+                    }
+                    System.out.println(1);
+                    System.exit(0);
+                }
+            }
+        }
+        System.out.println(0);
+    }
 }
