c98e03ad5eb2e41527a63d05413a2410ccf6c510
hek316 <yhr05008@naver.com>
2024-05-29 17:52:00 +0900

    BaekJoon 1260 - [600 - 그래프 1] DFS와 BFS

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-class Edge{
-    int from, to;
-
-    Edge(int from, int to){
-        this.from = from;
-        this.to = to;
-    }
-
-}
+import java.util.*;
 
 public class Main {
+    static ArrayList<Integer>[] a ;
 
-    static boolean[] visited;
-    static int answer;
-    static ArrayList<Integer>[] list;
-    public static void main(String[] args) throws IOException {
+    static boolean[] c;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void dfs(int x){
+        if(c[x]){
+            return;
+        }
+        c[x] = true;
+        sb.append(x).append(" ");
+        for(int y : a[x]){
+            if(c[y] == false){
+                dfs(y);
+            }
+        }
+    }
+
+    public static void bfs(int start){
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(start);
+        c[start] = true;
+        while (q.isEmpty() == false){
+            int x = q.remove();
+            sb.append(x).append(" ");
+            for(int i : a[x]){
+                if(c[i] == false){
+                    c[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[n];
-        //인접리스트
-        list = (ArrayList<Integer>[])new ArrayList[n];
-
-        for(int i=0; i<n; i++){
-            list[i] = new ArrayList<Integer>();
+        int start = Integer.parseInt(st.nextToken());
+        a = (ArrayList<Integer>[]) new ArrayList[n+1];
+        for(int i=0; i<=n; i++){
+            a[i] = new ArrayList<Integer>();
         }
-
-
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            list[from].add(to);
-            list[to].add(from);
+            int to= Integer.parseInt(st.nextToken());
+            a[from].add(to);
+            a[to].add(from);
         }
-
-        for(int i=0; i<n; i++){
-            if(answer != 1) dfs(1,i);
+        for(int i=1; i<=n; i++){
+            Collections.sort(a[i]);
         }
-        System.out.println(answer);
-
-    }
-
-    public static void dfs(int dept, int start){
-        if(dept == 5){
-            answer = 1;
-            return;
-        }
-        visited[start] = true;
-        for(int to :  list[start]){
-            if(visited[to] == false){
-                dfs(dept+1, to);
-            }
-        }
-        visited[start] = false;
+        c = new boolean[n+1];
+        dfs(start);
+        sb.append("\n");
+        c = new boolean[n+1];
+        bfs(start);
+        System.out.println(sb);
 
     }
 }
