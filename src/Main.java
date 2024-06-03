bfe1595835b0fbfc3a0d2e6e9cb468a71f42771a
hek316 <yhr05008@naver.com>
2024-06-03 22:51:30 +0900

    BaekJoon 7562 - [600 - 그래프 1] 나이트의 이동

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,30 @@ import java.util.*;
 
 public class Main {
 
-    // 세로
-    static int N;
-    // 가로
-    static int M;
     static int[][] arr;
-    static int[][] days;
+    static int[][] cnt;
 
 
-    static int[] dx = {-1, +1, 0, 0};
-    static int[] dy = {0, 0, +1, -1};
-
-    static Queue<int[]> q = new LinkedList<>();
-
-    static boolean all_ripen =true;
+    static int[] dx = {+1, +2, +1, +2, -2, -1, -2, -1};
+    static int[] dy = {+2, +1, -2, -1, +1, +2, -1, -2};
 
 
-    public static void bfs(){
+    public static void bfs(int[] start, int[] end, int l){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(start);
 
         while (!q.isEmpty()){
             int[] r = q.poll();
 
-            for(int i=0; i<4; i++){
+            for(int i=0; i<8; i++){
                 int x = r[0] + dx[i];
                 int y = r[1] + dy[i];
-                if(x>=0 && y>=0 && x < N && y < M && days[x][y] == 0 && arr[x][y] != 1){
-                    days[x][y] = days[r[0]][r[1]]+ 1;
+                if(x>=0 && y>=0 && x < l && y < l && cnt[x][y] == 0){
+                    cnt[x][y] = cnt[r[0]][r[1]]+ 1;
                     q.add(new int[]{x,y});
+                    if(x == end[0] && y == end[1]){
+                        return;
+                    }
                 }
             }
         }
@@ -41,47 +38,34 @@ public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
 
-        arr = new int[N][M];
-        days = new int[N][M];
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j] == 1){
-                    q.offer(new int[]{i,j});
-                } else if(arr[i][j] == -1){
-                    days[i][j] = -1;
-                } else if(arr[i][j] == 0){
-                    all_ripen = false;
-                }
-            }
-        }
-
-        if (all_ripen) {
-            System.out.println(0);
-            return;
-        }
-        bfs();
-        br.close();
-
-
-
-        int day = 0;
+        // 테스트 케이스 갯수
+        int N = Integer.parseInt(st.nextToken());
         for(int i=0; i<N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (days[i][j] == 0 && arr[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
+            // 체스판의 한 변의 길이
+            int l = Integer.parseInt(br.readLine());
+            arr = new int[l][l];
+            cnt = new int[l][l];
+            st = new StringTokenizer(br.readLine());
+            int[] start = new int[2];
+            start[0] = Integer.parseInt(st.nextToken());
+            start[1] = Integer.parseInt(st.nextToken());
 
-                day = Math.max(day, days[i][j]);
+            st = new StringTokenizer(br.readLine());
+            int[] end = new int[2];
+            end[0] = Integer.parseInt(st.nextToken());
+            end[1] = Integer.parseInt(st.nextToken());
+            if(start[0] == end[0] && start[1] == end[1]){
+                sb.append("0\n");
+            } else {
+                bfs(start, end,l);
+                sb.append(cnt[end[0]][end[1]]).append("\n");
             }
         }
 
-        System.out.println(day);
+        br.close();
+        System.out.println(sb);
 
     }
 
