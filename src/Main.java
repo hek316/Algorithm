b3dd65bdb9995ee0c9ccce5e2ea1387c7ee485c9
hek316 <yhr05008@naver.com>
2024-06-11 00:37:42 +0900

    BaekJoon 16929 - [601 - 그래프 1] (연습)] Two Dots / 풀이방법2

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,32 @@ import java.util.*;
 public class Main{
 
     static char[][] arr;
-    static int[][] cnts;
-
     static boolean[][] visited;
     static int N;
     static int M;
 
     static int[] dx = {+1, 0, -1, 0};
     static int[] dy = {0, +1, 0, -1};
+    static boolean result;
 
-    public static boolean dfs(int i, int j, int cnt){
-        if(visited[i][j]){ return cnt - cnts[i][j] >= 4;}
-        cnts[i][j] = cnt;
+    public static void dfs(int i, int j, int prei, int prej){
+        if(visited[i][j]){ return;}
         visited[i][j] = true;
         for(int m=0; m<4; m++){
             int x = i + dx[m];
             int y = j + dy[m];
             if(x >= 0 && y >= 0 && x < N && y < M && arr[i][j] == arr[x][y]){
-                if(dfs(x,y,cnt+1)){
-                    return true;
+                if(x == prei && y == prej){
+                    continue;
+                }
+                if(visited[x][y] == false){
+                    dfs(x,y, i, j);
+                } else if(visited[x][y] == true){
+                    result = true;
+                    return;
                 }
             }
         }
-        return false;
     }
     public static void main(String[] args) throws IOException {
 
@@ -38,7 +41,6 @@ public class Main{
         M = Integer.parseInt(st.nextToken());
 
         arr = new char[N][M];
-        cnts = new int[N][M];
         visited = new boolean[N][M];
         for(int i=0; i<N; i++){
             String s = br.readLine();
@@ -49,12 +51,81 @@ public class Main{
 
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
-                if(dfs(i,j,0)){
-                    System.out.println("Yes");
-                    return;
+                if(visited[i][j] == false){
+                    dfs(i,j, i,j);
+                    if(result){
+                        System.out.println( "Yes");
+                        return;
+                    }
                 }
             }
         }
         System.out.println("No");
+
+    }
+}import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.util.*;
+
+public class Main{
+
+    static char[][] arr;
+    static boolean[][] visited;
+    static int N;
+    static int M;
+
+    static int[] dx = {+1, 0, -1, 0};
+    static int[] dy = {0, +1, 0, -1};
+    static boolean result;
+
+    public static void dfs(int i, int j, int prei, int prej){
+        if(visited[i][j]){ return;}
+        visited[i][j] = true;
+        for(int m=0; m<4; m++){
+            int x = i + dx[m];
+            int y = j + dy[m];
+            if(x >= 0 && y >= 0 && x < N && y < M && arr[i][j] == arr[x][y]){
+                if(x == prei && y == prej){
+                    continue;
+                }
+                if(visited[x][y] == false){
+                    dfs(x,y, i, j);
+                } else if(visited[x][y] == true){
+                    result = true;
+                    return;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new char[N][M];
+        visited = new boolean[N][M];
+        for(int i=0; i<N; i++){
+            String s = br.readLine();
+            for(int j=0; j<M; j++){
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(visited[i][j] == false){
+                    dfs(i,j, i,j);
+                    if(result){
+                        System.out.println( "Yes");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("No");
+
     }
 }
