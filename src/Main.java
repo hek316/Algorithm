765621abd72df0980eb72f54bafd0ec74de8367d
hek316 <hek3167@gmail.com>
2024-06-18 00:30:09 +0900

    BaekJoon 2146 - [602 - 그래프 1 (도전)] 다리 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,99 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main{
+    static int[][] arr;
+    static int[][] cnt;
+    static boolean[][] visited;
+    static int[][] island;
     static int N;
-    static boolean[] check;
-    static ArrayList<Integer>[] arr;
-    static int[] order;
-    static int[] parents;
-    static int[] child;
-    static public void dfs(int idx){
-        if(check[idx]){
-            return;
-        }
-        check[idx] = true;
-        for(int y : arr[idx]){
-            if(check[y] == false){
-                child[idx] +=1;
-                parents[y] = idx;
-                dfs(y);
-            }
+    static int[] dx = {-1, +1, 0, 0};
+    static int[] dy = {0, 0, +1, -1};
+    static int MIN = Integer.MAX_VALUE;
 
+    static int num = 0;
+
+    static  Queue<Point> q = new LinkedList<>();
+
+    public static void findIsland(Point start){
+        Queue<Point> queue = new LinkedList<Point>();
+        queue.add(start);
+        num++;
+        visited[start.x][start.y] = true;
+        island[start.x][start.y] = num;
+        while (!queue.isEmpty()){
+            Point t = queue.remove();
+            for(int i=0; i<4; i++){
+                int a = t.x + dx[i];
+                int b = t.y + dy[i];
+                if(a >= 0 && b >= 0 && a < N && b < N){
+                    if(visited[a][b] == false && arr[a][b] == 1){
+                        island[a][b] = num;
+                        visited[a][b] = true;
+                        Point point2 = new Point(a, b);
+                        queue.add(point2);
+                    }
+                }
+            }
         }
     }
+    public static void bfs(){
+        while (!q.isEmpty()){
+            Point t = q.remove();
 
+            visited[t.x][t.y] = true;
+            for(int i=0; i<4; i++){
+                int a = t.x + dx[i];
+                int b = t.y + dy[i];
+                int land = island[t.x][t.y];
+                if(a >= 0 && b >= 0 && a < N && b < N){
+                    if(visited[a][b] == false && arr[a][b] == 0){
+                        cnt[a][b] = cnt[t.x][t.y] + 1;
+                        visited[a][b] = true;
+                        island[a][b] = land;
+                        Point point = new Point(a, b);
+                        q.add(point);
+                    } else if (visited[a][b] == true && land != island[a][b] ){
+                        MIN = Math.min(cnt[t.x][t.y] + cnt[a][b], MIN);
+                    }
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        arr = new ArrayList[N+1];
-        order = new int[N+1];
-        check = new boolean[N+1];
-        parents  = new int[N+1];
-        child = new int[N+1];
-        for(int i=0; i <=N; i++){
-            arr[i] = new ArrayList<Integer>();
-        }
+        arr = new int[N][N];
+        cnt = new int[N][N];
+        visited = new boolean[N][N];
+        island = new int[N][N];
 
-        for(int i=0; i < N-1; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            arr[u].add(v);
-            arr[v].add(u);
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=1; i <= N; i++){
-            order[i] = Integer.parseInt(st.nextToken());
-        }
-
-        if(order[1] != 1 ){
-            System.out.println("0");
-            return;
-        }
-
-        dfs(1);
-
-        Stack<Integer> stack = new Stack<>();
-        boolean result = findOrder(stack, 1);
-        if(result){
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
-    }
-
-    static public boolean findOrder(Stack<Integer> stack, int idx){
-        if(idx == N){
-            return true;
-        }
-        int x = order[idx];
-        if(!stack.isEmpty()){
-            int t = stack.peek();
-            int p = parents[x];
-            if(t != p){
-                return false;
-            }
-            child[p]--;
-            if(child[p] == 0){
-                stack.pop();
+            for (int j = 0; j < N; j++) {
+                int c = Integer.parseInt(st.nextToken());
+                arr[i][j] = c;
+                if (c == 1) {
+                    q.add(new Point(i, j));
+                }
             }
         }
-        if(child[x] != 0){
-            stack.add(x);
+        // 섬의 번호 부여
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 1 && island[i][j] == 0) {
+                    findIsland(new Point(i, j));
+                }
+            }
         }
-        if(findOrder(stack, idx+1) == false) return false;
-        else return true;
+        visited = new boolean[N][N];
+        bfs(); // bfs 를 통해 최소 다리 길이 찾기
+        System.out.println(MIN);
     }
 }
