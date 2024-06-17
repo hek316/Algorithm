f609b49dd87d22af6133d5fb8d0dab2dbfd1176f
hek316 <hek3167@gmail.com>
2024-06-17 14:56:39 +0900

    BaekJoon 16964 - [602 - 그래프 1 (도전)] DFS 스페셜 저지

--- src/Main.java
+++ src/Main.java
@@ -4,77 +4,91 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main{
-
-    static boolean[] visited;
-    static int[] child;
+    static int N;
+    static boolean[] check;
+    static ArrayList<Integer>[] arr;
+    static int[] order;
     static int[] parents;
+    static int[] child;
+    static public void dfs(int idx){
+        if(check[idx]){
+            return;
+        }
+        check[idx] = true;
+        for(int y : arr[idx]){
+            if(check[y] == false){
+                child[idx] +=1;
+                parents[y] = idx;
+                dfs(y);
+            }
 
-    static int[] result;
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        ArrayList<Integer>[] arr = new ArrayList[N+1];
-        visited = new boolean[N+1];
-        result = new int[N+1];
-        child  = new int[N+1];
-        parents = new int[N+1];
-
-        for(int i=0; i<=N; i++){
+        N = Integer.parseInt(st.nextToken());
+        arr = new ArrayList[N+1];
+        order = new int[N+1];
+        check = new boolean[N+1];
+        parents  = new int[N+1];
+        child = new int[N+1];
+        for(int i=0; i <=N; i++){
             arr[i] = new ArrayList<Integer>();
         }
 
-        for(int i=0; i<N-1; i++){
+        for(int i=0; i < N-1; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             arr[u].add(v);
             arr[v].add(u);
         }
+
         st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++){
-            result[i] = Integer.parseInt(st.nextToken());
+        for(int i=1; i <= N; i++){
+            order[i] = Integer.parseInt(st.nextToken());
         }
 
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(1);
-        visited[1] = true;
-
-        while (!q.isEmpty()) {
-            int t = q.remove();
-            for(int y : arr[t]){
-                if(visited[y] == false){
-                    parents[y] = t;
-                    child[t] +=1;
-                    q.add(y);
-                    visited[y] = true;
-                }
-            }
+        if(order[1] != 1 ){
+            System.out.println("0");
+            return;
         }
 
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(1);
+        dfs(1);
 
-        for(int i=2; i<=N; i++){
-            int n = result[i];
-            int t = queue.peek();
-            int p = parents[n];
-            if(t != p) {
-                System.out.println("0");
-                return;
-            } else {
-                child[t] = child[t] -1;
-                if(child[t] == 0){
-                    queue.remove();
-                }
+        Stack<Integer> stack = new Stack<>();
+        boolean result = findOrder(stack, 1);
+        if(result){
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+
+    static public boolean findOrder(Stack<Integer> stack, int idx){
+        if(idx == N){
+            return true;
+        }
+        int x = order[idx];
+        if(!stack.isEmpty()){
+            int t = stack.peek();
+            int p = parents[x];
+            if(t != p){
+                return false;
             }
-
-            if(child[n] != 0){
-                queue.add(n);
+            child[p]--;
+            if(child[p] == 0){
+                stack.pop();
             }
         }
-        System.out.println("1");
-    } 
+        if(child[x] != 0){
+            stack.add(x);
+        }
+        if(findOrder(stack, idx+1) == false) return false;
+        else return true;
+    }
 }
