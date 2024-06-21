4d74370a03b77938ab4d85b0989758926e03f1a9
hek316 <yhr05008@naver.com>
2024-06-21 20:27:35 +0900

    BaekJoon 14226 - [610 - BFS] 이모티콘

--- src/Main.java
+++ src/Main.java
@@ -4,104 +4,71 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main{
-    static int MAX = 100001;
-    static int[] dx = {-1 , +1};
-    static int[] seconds = new int[MAX];
-    static int[] parents = new int[MAX];
-    static boolean[] visited = new boolean[MAX];
-    static int start;
-    static int k;
 
-
-    static Queue<Integer> queue = new LinkedList<>();
-
-    // 시작점 < 목적지
-    static boolean leftside = false;
-
-    static Stack<Integer> stack;
-
-    public static void bfs(){
-        int cnt = 0;
-        while (!queue.isEmpty()){
-            int t = queue.remove();
-            cnt = seconds[t] + 1;
-            // 좌우로 한칸씩 이동
-            for(int i=0; i<2; i++){
-                int next = t + dx[i];
-                if(next >=0 && next < MAX){
-                    if(visited[next] == false){
-                        parents[next] = t;
-                        visited[next] = true;
-                        seconds[next] = cnt;
-                        if(next == k){
-                            return;
-                        }
-                        if((leftside && next < k )|| (!leftside && k < next )){
-                            queue.add(next);
-                        }
-                    }
-                }
-            }
-            // 2*x 칸으로 이동
-            int x2 = 2*t;
-
-            if(x2 < MAX){
-                if( visited[x2] == false){
-                    seconds[x2] = cnt;
-                    parents[x2] = t;
-                    if(x2 == k){
-                        return;
-                    }
-                    if(visited[x2] == false){
-                        if(Math.abs(x2-k) + cnt < Math.abs(start-k)){
-                            visited[x2] = true;
-                            queue.add(x2);
-                        }
-                    }
-                }
-            }
-
+    public static class Emotion{
+        int screen;
+        int clipboard;
+        int cnt;
+        public Emotion(int screen, int clipboard, int cnt){
+            this.screen = screen;
+            this.clipboard = clipboard;
+            this.cnt = cnt;
         }
     }
 
-    public static void dfs(int k){
-        stack.push(k);
-        if( k == start){
-            return;
-        }
-        dfs(parents[k]);
-    }
+    static int goal;
+    static int MAX = 1001;
+    static boolean[][] visit;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        start = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        stack = new Stack<Integer>();
-        StringBuilder sb = new StringBuilder();
-        if(start == k){
-            sb.append("0\n").append(start);
-            System.out.println(sb);
-            return;
+        goal = Integer.parseInt(st.nextToken());
+        visit = new boolean[MAX][MAX];
+
+        Queue<Emotion> queue = new LinkedList<Emotion>();
+
+        queue.add(new Emotion(1,0,0));
+        visit[1][0] = true;
+
+        while (!queue.isEmpty()){
+            // s : 화면의 이모티콘 수
+            // c : 클립복드의 이모티콘 수
+            Emotion cur = queue.remove();
+            int screen = cur.screen;
+            // copy
+            if(screen < MAX && visit[screen][screen] == false){
+                visit[screen][screen] = true;
+                if (screen == goal) {
+                    System.out.println(cur.cnt + 1);
+                    return;
+                }
+                queue.add(new Emotion(screen,screen,cur.cnt+ 1));
+            }
+
+            screen = cur.screen + cur.clipboard;
+            // paste
+            if(screen < MAX && visit[screen][cur.clipboard] == false){
+                visit[screen][cur.clipboard] = true;
+                if (screen == goal) {
+                    System.out.println(cur.cnt + 1);
+                    return;
+                }
+                queue.add(new Emotion(screen, cur.clipboard, cur.cnt + 1));
+            }
+
+            screen = cur.screen - 1;
+            // minus
+            if(screen > 0 && visit[screen][cur.clipboard] == false){
+                visit[screen][cur.clipboard] = true;
+                if (screen == goal) {
+                    System.out.println(cur.cnt + 1);
+                    return;
+                }
+                queue.add(new Emotion(screen, cur.clipboard, cur.cnt + 1));
+            }
         }
-
-        if (start < k){
-            leftside = true;
-        }
-
-        queue.add(start);
-        visited[start] = true;
-
-        bfs();
-        dfs(k);
-
-        while (!stack.isEmpty()){
-            sb.append(stack.pop()).append(" ");
-        }
-        System.out.println(seconds[k]);
-        System.out.println(sb);
-
 
     }
 }
