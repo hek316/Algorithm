be5fedcc64baca4875d12921b71808e2a43a7482
hek316 <yhr05008@naver.com>
2024-06-24 16:07:34 +0900

    BaekJoon 1261 - [610 - BFS] 알고스팟 - 풀이 2 덱

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,76 @@
-import java.awt.*;
 import java.io.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+
+class Point{
+    int x,y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 public class Main{
-    static int N;
-    static int M;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] cnt;
+
 
     public static void main(String[] arg) throws IOException{
+
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        char[][] arr = new char[N][M];
-        cnt = new int[N][M];
-        for(int i=0; i<N; i++){
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        char[][] arr = new char[n][m];
+
+        for(int i=0; i<n; i++){
             String s = br.readLine();
-            for(int j=0; j<M; j++){
+            for(int j=0; j<m; j++){
                 arr[i][j] = s.charAt(j);
             }
         }
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                cnt[i][j] = -1;
+        int[][] d = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                d[i][j] = -1;
             }
         }
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(0, 0));
-        cnt[0][0] = 0;
-        while(!queue.isEmpty()){
-            Point p = queue.remove();
+
+        Queue<Point> q = new LinkedList<Point>();
+        Queue<Point> next_q = new LinkedList<Point>();
+        q.offer(new Point(0, 0));
+        d[0][0] = 0;
+        while(!q.isEmpty()){
+            Point p = q.remove();
             for(int i=0; i<4; i++){
                 int x = p.x + dx[i];
                 int y = p.y + dy[i];
-                if(x >= 0 && y >= 0 && x < N && y < M){
-                    int next = 0;
-                    if(arr[x][y] == '1'){
-                        next = cnt[p.x][p.y] + 1;
-                    } else {
-                        next = cnt[p.x][p.y];
-                    }
-                    if(cnt[x][y] == -1  || cnt[x][y] != -1 && cnt[x][y] > next){
-                        queue.add(new Point(x,y));
-                        cnt[x][y] = next;
-                    }
+                if(x >= 0 && y >= 0 && x < n && y < m){
+                    if(d[x][y] == -1){
+                        if(arr[x][y] == '0'){
+                            d[x][y] = d[p.x][p.y];
+                            q.offer(new Point(x, y));
+                        } else {
+                            d[x][y] = d[p.x][p.y]+1;
+                            next_q.add(new Point(x, y));
+                        }
 
+                    }
                 }
+            }
+            if(q.isEmpty()){
+                q = next_q;
+                next_q = new LinkedList<Point>();
             }
 
         }
-
-        System.out.println(cnt[N-1][M-1]);
+        System.out.println(d[n-1][m-1]);
 
     }
 }
