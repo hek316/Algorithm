13f50cdd563691707c7956710168bddcd0be6b69
hek316 <yhr05008@naver.com>
2024-06-24 16:06:58 +0900

    BaekJoon 1261 - [610 - BFS] 알고스팟 - 풀이 1 Queue

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,62 @@
+import java.awt.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
     static int N;
-    static int K;
-    static int MIN = Integer.MAX_VALUE;
+    static int M;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] cnt;
 
     public static void main(String[] arg) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        dfs(K,0);
-        System.out.println(MIN);
+        char[][] arr = new char[N][M];
+        cnt = new int[N][M];
+        for(int i=0; i<N; i++){
+            String s = br.readLine();
+            for(int j=0; j<M; j++){
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                cnt[i][j] = -1;
+            }
+        }
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(0, 0));
+        cnt[0][0] = 0;
+        while(!queue.isEmpty()){
+            Point p = queue.remove();
+            for(int i=0; i<4; i++){
+                int x = p.x + dx[i];
+                int y = p.y + dy[i];
+                if(x >= 0 && y >= 0 && x < N && y < M){
+                    int next = 0;
+                    if(arr[x][y] == '1'){
+                        next = cnt[p.x][p.y] + 1;
+                    } else {
+                        next = cnt[p.x][p.y];
+                    }
+                    if(cnt[x][y] == -1  || cnt[x][y] != -1 && cnt[x][y] > next){
+                        queue.add(new Point(x,y));
+                        cnt[x][y] = next;
+                    }
+
+                }
+            }
+
+        }
+
+        System.out.println(cnt[N-1][M-1]);
 
     }
-    static void dfs(int k, int time) {
-        if(N >= k){
-            MIN = Math.min(MIN, time+ N-k);
-            return;
-        }
-
-        dfs(N, time + k - N);
-
-
-        if(N==0 && k <2){
-            MIN = Math.min(MIN, time+ k);
-            return;
-        }
-
-        if(k%2 == 0){
-            dfs(k/2, time);
-        } else{
-            dfs(k+1, time+1);
-            dfs(k-1, time+1);
-        }
-    }
-
 }
