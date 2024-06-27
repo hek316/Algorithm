3ce63060938f93f966fe516eb5dd11ceda1c2faf
hek316 <yhr05008@naver.com>
2024-06-27 18:00:07 +0900

    BaekJoon 2250 - [620 - 트리 1] 트리의 지름 - 풀이방법1 (dfs)

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,85 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
+class Node{
+    int v, dist;
+    public Node(int v, int dist){
+        this.v = v;
+        this.dist = dist;
+    }
+}
+
 public class Main{
-    static ArrayList<Integer>[] arr ;
+    static ArrayList<Node>[] arr ;
+    static  int[] deptArr;
+    static  int[] parents;
+    static  int[] distArr;
+    static int MAX = 0;
+    static Stack<Integer> stack = new Stack<Integer>();
 
     public static void main(String[] arg) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         arr = new ArrayList[n+1];
+        deptArr = new int[n+1];
+        parents = new int[n+1];
+        distArr = new int[n+1];
         for(int i=0; i<=n; i++){
-            arr[i] = new ArrayList<Integer>();
+            arr[i] = new ArrayList<Node>();
         }
 
         StringTokenizer st;
-        for(int i=0; i<n-1; i++){
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            arr[u].add(v);
-            arr[v].add(u);
-        }
-        int[] parents = new int[n+1];
-        for(int i=0; i<parents.length; i++){
-            parents[i] = -1;
-        }
-
-
-        Queue<Integer> queue = new LinkedList<Integer>();
-        for(int y: arr[1]){
-            queue.add(y);
-            parents[y] = 1;
-        }
-        while (!queue.isEmpty()){
-            int t = queue.remove();
-            for(int y: arr[t]){
-                if(parents[y] == -1){
-                    parents[y] = t;
-                    queue.add(y);
-                }
+            while (true){
+                int v = Integer.parseInt(st.nextToken());
+                if(v == -1) break;
+                int dist = Integer.parseInt(st.nextToken());
+                arr[u].add(new Node(v,dist));
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=2; i<=n; i++){
-            sb.append(parents[i]).append("\n");
+        dfs(1,-1,1);
+
+        int[] deptSum = new int[n+1];
+
+        // 같은 레벨끼리 더하기
+        int[] levelCompare = new int[n+1];
+        while (!stack.isEmpty()){
+            // 노드 꺼내기
+            int pop = stack.pop();
+            int parent = parents[pop];
+            if (parent == 0){
+                System.out.println(MAX);
+                return;}
+            if( deptSum[pop] == 0){
+                deptSum[pop] = distArr[pop];
+            }
+            int tmp = deptSum[pop] + distArr[parent];
+            deptSum[parent] = Math.max(tmp, deptSum[parent]);
+            int childMax =levelCompare[parent] +deptSum[pop] ;
+            // 같은 레벨에서 더한 값 비교
+            MAX = Math.max(MAX,  childMax);
+            // 같은 부모중에 가장 큰 자식 고르기
+            levelCompare[parent] = Math.max(deptSum[pop], levelCompare[parent]);
+            MAX = Math.max(MAX,  deptSum[parent]);
         }
-        System.out.println(sb);
+    }
+
+    static void dfs(int root, int pre, int dept){
+        stack.push(root);
+        for(Node y : arr[root]){
+            if(y.v == pre){
+                continue;
+            }
+            deptArr[y.v] = dept;
+            parents[y.v] = root;
+            distArr[y.v] = y.dist;
+
+            dfs(y.v, root, dept+1);
+        }
     }
 }
